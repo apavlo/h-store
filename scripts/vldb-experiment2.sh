483dde7430c531a61fd30636060c81c59a247706
Andy Pavlo <pavlo@cs.brown.edu>
2010-11-08 18:36:47 +0000

    Updated to include tm1 and renamed ebay->auctionmark
    git-svn-id: https://database.cs.brown.edu/svn/hstore/trunk@1674 7a8f3d5b-8c5f-44cb-9125-2414a256df87

--- scripts/vldb-experiment2.sh
+++ scripts/vldb-experiment2.sh
@@ -1,17 +1,19 @@
 #!/bin/bash -x
 
 BENCHMARKS=( \
+    "tm1"\
     "tpce"\
     "tpcc.100w"\
     "tpcc.50w"\
-    "ebay"\
+    "auctionmark"\
 )
 HSTORE_LOCAL_TIME=1200
 MEMORY=3600
 
+tm1_mix="DeleteCallForwarding:2,GetAccessData:35,GetNewDestination:10,GetSubscriberData:35,InsertCallForwarding:2,UpdateLocation:14,UpdateSubscriberData:2"
 tpce_mix="BrokerVolume:5,CustomerPosition:13,MarketFeed:1,MarketWatch:18,SecurityDetail:14,TradeLookup:8,TradeOrder:10,TradeResult:10,TradeStatus:19,TradeUpdate:2,DataMaintenance:1,TradeCleanup:1"
 tpcc_mix="delivery:4,neworder:45,slev:4,ostatByCustomerId:2,ostatByCustomerName:3,paymentByCustomerIdC:13,paymentByCustomerIdW:13,paymentByCustomerNameC:8,paymentByCustomerNameW:9"
-ebay_mix="CheckWinningBids:-1,GetItem:35,GetUserInfo:10,GetWatchedItems:10,NewBid:13,NewComment:2,GetComment:2,NewCommentResponse:1,NewFeedback:5,NewItem:10,NewPurchase:2,NewUser:8,PostAuction:-1,UpdateItem:2"
+auctionmark_mix="CheckWinningBids:-1,GetItem:35,GetUserInfo:10,GetWatchedItems:10,NewBid:13,NewComment:2,GetComment:2,NewCommentResponse:1,NewFeedback:5,NewItem:10,NewPurchase:2,NewUser:8,PostAuction:-1,UpdateItem:2"
 
 benchmark=$1
 if [ "$2" = "1" ]; then
@@ -31,8 +33,10 @@ if [ "$benchmark" = "tpcc.100w" -o "$benchmark" = "tpcc.50w" ]; then
     txn_mix=$tpcc_mix
 elif [ "$benchmark" = "tpce" ]; then
     txn_mix=$tpce_mix
-elif [ "$benchmark" = "ebay" ]; then
-    txn_mix=$ebay_mix
+elif [ "$benchmark" = "auctionmark" ]; then
+    txn_mix=$auctionmark_mix
+elif [ "$benchmark" = "tm1" ]; then
+    txn_mix=$tm1_mix
 else
     echo "ERROR: Invalid benchmark '$benchmark'"
     exit 1
