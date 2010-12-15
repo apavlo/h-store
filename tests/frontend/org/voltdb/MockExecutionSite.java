3226d79aacde2a6b26d9cb0e8b3f85ab44bfc942
Dylan Wu <sw47@cs.brown.edu>
2010-12-15 18:21:16 +0000

    Implemented new HStoreMessenger test cases for sending/receiving data between sites/partitions. Good times
    git-svn-id: https://database.cs.brown.edu/svn/hstore/trunk@1766 7a8f3d5b-8c5f-44cb-9125-2414a256df87

--- tests/frontend/org/voltdb/MockExecutionSite.java
+++ tests/frontend/org/voltdb/MockExecutionSite.java
@@ -3,6 +3,8 @@ package org.voltdb;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 import org.voltdb.catalog.Catalog;
 import org.voltdb.messaging.FragmentTaskMessage;
@@ -18,6 +20,7 @@ public class MockExecutionSite extends ExecutionSite {
     private static final BackendTarget BACKEND_TARGET = BackendTarget.HSQLDB_BACKEND;
     
     private final Map<Long, VoltTable> dependencies = new HashMap<Long, VoltTable>();
+    private final Map<Long, CountDownLatch> latches = new HashMap<Long, CountDownLatch>();
     
     public MockExecutionSite(int partition_id, Catalog catalog, PartitionEstimator p_estimator) {
         super(partition_id, catalog, BACKEND_TARGET, p_estimator, null);
@@ -39,12 +42,37 @@ public class MockExecutionSite extends ExecutionSite {
     }
     
     @Override
-    public void storeDependency(long txnId, int senderPartitionId, int dependencyId, VoltTable data) {
+    public synchronized void storeDependency(long txnId, int senderPartitionId, int dependencyId, VoltTable data) {
+    	System.err.println("STORING TXN #" + txnId);
         this.dependencies.put(txnId, data);
+        CountDownLatch latch = this.latches.get(txnId);
+        if (latch != null) {
+        	System.err.println("UNBLOCKING TXN #" + txnId);
+        	latch.countDown();
+        }
     }
     
-    public VoltTable getDependency(long txnId) {
+    public synchronized VoltTable getDependency(long txnId) {
         return this.dependencies.get(txnId);
+    }
+    
+    public synchronized VoltTable waitForDependency(long txnId) {
+    	VoltTable vt = this.dependencies.get(txnId);
+    	if (vt == null) {
+    		CountDownLatch latch = this.latches.get(txnId);
+    		if (latch == null) {
+    			latch = new CountDownLatch(1);
+    			this.latches.put(txnId, latch);
+    		}
+    		try {
+    			System.err.println("WAITING FOR TXN #" + txnId);
+    			latch.await(100, TimeUnit.MILLISECONDS);
+    		} catch (InterruptedException ex) {
+    			throw new RuntimeException(ex);
+    		}
+    		vt = this.dependencies.get(txnId);
+    	}
+    	return (vt);
     }
     
     @Override
