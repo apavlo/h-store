075d12d8afaaaaeda040db4836afc5bce844b0ca
Andy Pavlo <pavlo@cs.brown.edu>
2011-01-14 02:00:05 +0000

    Rewrite of PlannerContext to now check for duplicate PlanColumns *before* assigning them a new GUID. Added a new test case to check that duplicate columns always get the same handle. Added recursive ExpressionUtil.equals() method to check whether expression trees are the same in PlannerContext.
    git-svn-id: https://database.cs.brown.edu/svn/hstore/trunk@1819 7a8f3d5b-8c5f-44cb-9125-2414a256df87

--- tests/frontend/edu/brown/expressions/TestExpressionUtil.java
+++ tests/frontend/edu/brown/expressions/TestExpressionUtil.java
@@ -1,10 +1,16 @@
 package edu.brown.expressions;
 
+import java.util.Set;
+
+import org.json.JSONObject;
+import org.junit.Test;
 import org.voltdb.catalog.*;
 import org.voltdb.expressions.AbstractExpression;
+import org.voltdb.expressions.NullValueExpression;
 import org.voltdb.plannodes.*;
 
 import edu.brown.BaseTestCase;
+import edu.brown.benchmark.tm1.procedures.DeleteCallForwarding;
 import edu.brown.catalog.QueryPlanUtil;
 import edu.brown.plannodes.PlanNodeUtil;
 import edu.brown.utils.CollectionUtil;
@@ -12,12 +18,55 @@ import edu.brown.utils.ProjectType;
 
 public class TestExpressionUtil extends BaseTestCase {
 
-    public static final String TARGET_PROC = "DeleteCallForwarding";
+    public static final String TARGET_PROC = DeleteCallForwarding.class.getSimpleName();
     public static final String TARGET_STMT = "query";
+
+    private Procedure catalog_proc;
+    private Statement catalog_stmt;
     
     @Override
     protected void setUp() throws Exception {
         super.setUp(ProjectType.TM1);
+        
+        this.catalog_proc = this.getProcedure(TARGET_PROC);
+        this.catalog_stmt = this.getStatement(this.catalog_proc, TARGET_STMT);
+    }
+    
+    /**
+     * testEquals
+     */
+    @Test
+    public void testEquals() throws Exception {
+        AbstractPlanNode root = QueryPlanUtil.deserializeStatement(catalog_stmt, true);
+        assertNotNull(root);
+        
+        Set<AbstractScanPlanNode> scan_nodes = PlanNodeUtil.getPlanNodes(root, AbstractScanPlanNode.class);
+        assertNotNull(scan_nodes);
+        assertEquals(1, scan_nodes.size());
+        AbstractScanPlanNode scan_node = CollectionUtil.getFirst(scan_nodes);
+        assertNotNull(scan_node);
+        
+        Set<AbstractExpression> exps = PlanNodeUtil.getExpressions(scan_node);
+        assertNotNull(exps);
+        assertFalse(exps.isEmpty());
+        AbstractExpression exp = CollectionUtil.getFirst(exps);
+        assertNotNull(exp);
+        
+        // Clone the mofo and make sure equals() returns true!
+        String json = exp.toJSONString();
+        assertFalse(json.isEmpty());
+        AbstractExpression clone = AbstractExpression.fromJSONObject(new JSONObject(json), catalog_db);
+        assertNotNull(clone);
+        assert(ExpressionUtil.equals(exp, clone));
+
+        // Change one of the branches. This should now return false!
+        clone.setRight(new NullValueExpression());
+        assertFalse(ExpressionUtil.equals(exp, clone));
+        
+        // Remove both the branch on both sids. This should return true!
+        exp.setRight(null);
+        clone.setRight(null);
+        assert(ExpressionUtil.equals(exp, clone));
     }
     
     /**
@@ -25,10 +74,6 @@ public class TestExpressionUtil extends BaseTestCase {
      */
     public void testDebug() throws Exception {
         // Just make sure this doesn't throw an Exception
-        Procedure catalog_proc = this.getProcedure(TARGET_PROC);
-        Statement catalog_stmt = catalog_proc.getStatements().get(TARGET_STMT);
-        assertNotNull(catalog_stmt);
-        
         AbstractPlanNode root = QueryPlanUtil.deserializeStatement(catalog_stmt, true);
         assertNotNull(root);
         //System.err.println(PlanNodeUtil.debug(root));
