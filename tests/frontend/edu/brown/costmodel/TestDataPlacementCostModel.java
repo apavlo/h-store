3465f31a963729728787ed79151f23d2ef528fb5
Dylan Wu <sw47@cs.brown.edu>
2011-03-13 22:49:12 +0000

    NewOrder trace with only one batch (Cluster configuration is: 100 host, 1 site, 1 partition)
    git-svn-id: https://database.cs.brown.edu/svn/hstore/branches/placement-branch@2003 7a8f3d5b-8c5f-44cb-9125-2414a256df87

--- tests/frontend/edu/brown/costmodel/TestDataPlacementCostModel.java
+++ tests/frontend/edu/brown/costmodel/TestDataPlacementCostModel.java
@@ -19,6 +19,7 @@ import org.voltdb.types.ExpressionType;
 import sun.security.acl.WorldGroupImpl;
 
 import edu.brown.catalog.CatalogUtil;
+import edu.brown.catalog.FixCatalog;
 import edu.brown.utils.ProjectType;
 import edu.brown.workload.TransactionTrace;
 import edu.brown.workload.Workload;
@@ -32,14 +33,13 @@ import edu.brown.workload.filters.ProcedureNameFilter;
 public class TestDataPlacementCostModel extends BaseTestCase {
     private static final Logger LOG = Logger.getLogger(TestDataPlacementCostModel.class);
 
-    private static final int WORKLOAD_XACT_LIMIT = 1;
+    private static final int WORKLOAD_XACT_LIMIT = 500;
     private static final int PROC_COUNT = 1;
     
-    private static final int NUM_HOSTS = 1;
-    private static final int NUM_SITES = 1;
+    private static final int NUM_HOSTS = 100;
+    private static final int NUM_SITES = 2;
     private static final int NUM_PARTITIONS = 1;
     private static final int BASE_PARTITION = 1;
-    private static TransactionTrace multip_trace;
     private static Procedure catalog_proc;
 
     private static final Class<? extends VoltProcedure> TARGET_PROCEDURE = neworder.class;
@@ -52,26 +52,32 @@ public class TestDataPlacementCostModel extends BaseTestCase {
         //super.setUp(ProjectType.LOCALITY);
         super.setUp(ProjectType.TPCC);
         
-        LOG.info("BEFORE HOSTS: " + CatalogUtil.getNumberOfHosts(catalog_db));
-        LOG.info("BEFORE SITES: " + CatalogUtil.getNumberOfSites(catalog_db));
-        LOG.info("BEFORE PARTITIONS: " + CatalogUtil.getNumberOfPartitions(catalog_db));
-        
-        this.initializeCluster(NUM_HOSTS, NUM_SITES, NUM_PARTITIONS);
-        
-        LOG.info("AFTER HOSTS: " + CatalogUtil.getNumberOfHosts(catalog_db));
-        LOG.info("AFTER SITES: " + CatalogUtil.getNumberOfSites(catalog_db));
-        LOG.info("AFTER PARTITIONS: " + CatalogUtil.getNumberOfPartitions(catalog_db));
+//        LOG.info("BEFORE HOSTS: " + CatalogUtil.getNumberOfHosts(catalog_db));
+//        LOG.info("BEFORE SITES: " + CatalogUtil.getNumberOfSites(catalog_db));
+//        LOG.info("BEFORE PARTITIONS: " + CatalogUtil.getNumberOfPartitions(catalog_db));
+//        
+//        this.initializeCluster(NUM_HOSTS, NUM_SITES, NUM_PARTITIONS);
+//        
+//        LOG.info("AFTER HOSTS: " + CatalogUtil.getNumberOfHosts(catalog_db));
+//        LOG.info("AFTER SITES: " + CatalogUtil.getNumberOfSites(catalog_db));
+//        LOG.info("AFTER PARTITIONS: " + CatalogUtil.getNumberOfPartitions(catalog_db));
         
         //this.addPartitions(NUM_PARTITIONS);
         // Super hack! Walk back the directories and find out workload directory
+    }
+
+    /**
+     * testEstimateCost
+     */
+    public void testEstimateCostMultiPartition() throws Exception {
+        // set the cluster configuration
+        this.initializeCluster(100, 1, 1);
         if (workload == null) {
             //File workload_file = this.getWorkloadFile(ProjectType.LOCALITY); 
-            File file = this.getWorkloadFile(ProjectType.TPCC, "100w.large"); 
+            File file = this.getWorkloadFile(ProjectType.TPCC); 
             workload = new Workload(catalog);
             catalog_proc = this.getProcedure(TARGET_PROCEDURE);
             
-//            ProcedureLimitFilter filter = new ProcedureLimitFilter(WORKLOAD_COUNT);
-//            workload.load(workload_file.getAbsolutePath(), catalog_db, filter);
             
             // Check out this beauty:
             // (1) Filter by procedure name
@@ -82,26 +88,25 @@ public class TestDataPlacementCostModel extends BaseTestCase {
             LOG.info("filter starting to apply filter");
             Workload.Filter filter = new ProcedureNameFilter()
                     .include(TARGET_PROCEDURE.getSimpleName())
-//                    .attach(new ProcParameterValueFilter().include(1, new Long(5))) // D_ID
-//                    .attach(new ProcParameterArraySizeFilter(CatalogUtil.getArrayProcParameters(catalog_proc).get(0), 10, ExpressionType.COMPARE_EQUAL))
-                    .attach(new BasePartitionTxnFilter(p_estimator, BASE_PARTITION))
-                    .attach(new MultiPartitionTxnFilter(p_estimator))
-                    .attach(new ProcedureLimitFilter(WORKLOAD_XACT_LIMIT));
+                    .attach(new BasePartitionTxnFilter(p_estimator, BASE_PARTITION));
+//                    .attach(new MultiPartitionTxnFilter(p_estimator));
+//                    .attach(new ProcedureLimitFilter(WORKLOAD_XACT_LIMIT));
             LOG.info("filter: " + filter + " catalogdb: " + (catalog_db));
             workload.load(file.getAbsolutePath(), catalog_db, filter);
         }
         assert(workload.getTransactionCount() > 0);
-    }
-
-    /**
-     * testEstimateCost
-     */
-    public void testEstimateCostMultiPartition() throws Exception {
+        LOG.info("transactions in workload: " + workload.getTransactionCount());
+        
+        LOG.info("AFTER HOSTS: " + CatalogUtil.getNumberOfHosts(catalog_db));
+        LOG.info("AFTER SITES: " + CatalogUtil.getNumberOfSites(catalog_db));
+        LOG.info("AFTER PARTITIONS: " + CatalogUtil.getNumberOfPartitions(catalog_db));
+        
         LOG.info("num of transactions: " + workload.getTransactionCount());
         System.err.println(workload.getTransactions().get(0).debug(catalog_db));
         
         // Now calculate cost of touching these partitions
         TransactionTrace txn_trace = workload.getTransactions().get(0);
+        LOG.info("transaction trace: " + txn_trace);
         assertNotNull(txn_trace);
         DataPlacementCostModel cost_model = new DataPlacementCostModel(catalog_db);
         cost_model.prepare(catalog_db);
