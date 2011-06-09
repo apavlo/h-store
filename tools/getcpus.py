11399f0860edbd74dea32b9599b743f060f7354e
Andy Pavlo <pavlo@cs.brown.edu>
2011-06-09 23:36:15 +0000

    Added a ant target 'getcpus' that allows us to set the number of threads to use when building the Dtxn.Coordinator, the TPC-E EGenerator, and Google ProtocolBuffers. This should provide a small speed improvement. Fixed failing test cases by initializing HStoreConf in BaseTestCase.
    git-svn-id: https://database.cs.brown.edu/svn/hstore/branches/markov-branch@2138 7a8f3d5b-8c5f-44cb-9125-2414a256df87

--- tools/getcpus.py
+++ tools/getcpus.py
@@ -4,11 +4,12 @@
 import os, sys, commands, string
 
 numHardwareThreads = 4
-if CTX.PLATFORM == "Darwin":
+platform = os.uname()[0]
+if platform == "Darwin":
     numHardwareThreads = 0
     output = commands.getstatusoutput("sysctl hw.ncpu")
     numHardwareThreads = int(string.strip(string.split(output[1])[1]))
-elif CTX.PLATFORM == "Linux":
+elif platform == "Linux":
     numHardwareThreads = 0
     for line in open('/proc/cpuinfo').readlines():
         name_value = map(string.strip, string.split(line, ':', 1))
