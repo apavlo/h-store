c5c743ad857bc62c12be6defd33a4e29d8124d10
Andy Pavlo <pavlo@cs.brown.edu>
2011-06-20 19:46:47 +0000

    Working on improved Comet input generator. Doesn't work for TPC-C
    git-svn-id: https://database.cs.brown.edu/svn/hstore/branches/cjc-branch@2165 7a8f3d5b-8c5f-44cb-9125-2414a256df87

--- src/frontend/edu/brown/designer/placement/TransformTransactionTraces.java
+++ src/frontend/edu/brown/designer/placement/TransformTransactionTraces.java
@@ -15,7 +15,6 @@ import edu.brown.statistics.Histogram;
 import edu.brown.utils.ArgumentsParser;
 import edu.brown.utils.FileUtil;
 import edu.brown.utils.PartitionEstimator;
-import edu.brown.utils.StringUtil;
 import edu.brown.workload.QueryTrace;
 import edu.brown.workload.TransactionTrace;
 
@@ -28,29 +27,25 @@ public class TransformTransactionTraces {
     private static final double DEFAULT_DIFFERENT_SITE_PENALTY      = 100d;
     private static final double DEFAULT_DIFFERENT_HOST_PENALTY      = 10000d;
     
-    protected static class TxnPartition {
-        final int basePartition;
-        final List<Set<Integer>> partitions = new ArrayList<Set<Integer>>();
-        
-        public TxnPartition(int basePartition) {
-            this.basePartition = basePartition;
-        }
-        
-        public List<Set<Integer>> getPartitions() {
-            return partitions;
-        }
-        
-        public String toString() {
-            StringBuilder sb = new StringBuilder();
-            sb.append("base partition: " + String.valueOf(basePartition) + " ");
-            for (Set<Integer> batch : partitions) {
-                for (Integer partition_id : batch) {
-                    sb.append(String.valueOf(partition_id) + " ");
-                }
-            }
-            return sb.toString();
-        }
-    }
+//    private static class TxnPartition {
+//        final int basePartition;
+//        final List<Set<Integer>> partitions = new ArrayList<Set<Integer>>();
+//        
+//        public TxnPartition(int basePartition) {
+//            this.basePartition = basePartition;
+//        }
+//        
+//        public String toString() {
+//            StringBuilder sb = new StringBuilder();
+//            sb.append("base partition: " + String.valueOf(basePartition) + " ");
+//            for (Set<Integer> batch : partitions) {
+//                for (Integer partition_id : batch) {
+//                    sb.append(String.valueOf(partition_id) + " ");
+//                }
+//            }
+//            return sb.toString();
+//        }
+//    }
     
     /**
      * File format like the following:
@@ -73,76 +68,82 @@ public class TransformTransactionTraces {
         final int total_num_partitions = CatalogUtil.getNumberOfPartitions(catalogDb);
         final int sites_per_host = CatalogUtil.getNumberOfSites(catalogDb) / CatalogUtil.getNumberOfHosts(catalogDb);
         
-        Histogram<Integer> hist = new Histogram<Integer>();
-        List<TxnPartition> al_txn_partitions = new ArrayList<TxnPartition>();
-
-        /** keep the count on the number of times that each transaction access a partition per batch. **/
-        TxnPartition txn_partitions;
-
         int total_num_transactions = txn_traces.size(); // ???
-        int[][] output_matrix = new int[total_num_partitions][total_num_partitions];
-        int[] num_partition_batches = new int[total_num_partitions];
+        int[][] affinity = new int[total_num_partitions][total_num_partitions];
+        Histogram<Integer> multipartition_batches = new Histogram<Integer>();
+        
+        Set<Integer> all_partitions = new HashSet<Integer>();
+        Histogram<Integer> heat_histogram = new Histogram<Integer>();
         
         for (TransactionTrace trace : txn_traces) {
             int base_partition = 0;
             try {
                 base_partition = est.getBasePartition(trace);
-                hist.put(base_partition);
             } catch (Exception e1) {
                 throw new RuntimeException(e1);
             }
-            txn_partitions = new TxnPartition(base_partition);
+            
+            all_partitions.clear();
+            all_partitions.add(base_partition);
+            affinity[base_partition][base_partition]++;
             
             for (Integer batch_id : trace.getBatchIds()) {
                 Set<Integer> partitions_touched_per_batch = new HashSet<Integer>();
-                List<QueryTrace> batch_queries = trace.getBatches().get(batch_id);
                 
-                // list of query traces
-                Set<Integer> total_query_partitions = new HashSet<Integer>();
-                if (isMultiPartitionBatch(batch_queries, est, base_partition)) {
-                    for (QueryTrace qt : batch_queries) {
-                        try {
-                        	Set<Integer> batch_partitions = est.getAllPartitions(qt, base_partition);
-                        	for (int partition : batch_partitions) {
-                    			if (!partitions_touched_per_batch.contains(partition)) {
-                    				partitions_touched_per_batch.add(partition);
-                    			}
-                        	}                    		
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    }
-                	// increment # of multi-partition batches from the base partition
-                    num_partition_batches[base_partition]++;
-                	// calculate which other partitions were touched by the multi partition transaction
-                    output_matrix[base_partition][base_partition]++;
-                    for (int partition_num : partitions_touched_per_batch) {
-                    	//output_matrix[base_partition][partition_num]++;                    		
-                    	if (partition_num != base_partition) {
-                        	output_matrix[base_partition][partition_num]++;                    		
+                Set<Integer> batch_partitions = null;
+                for (QueryTrace qt : trace.getBatches().get(batch_id)) {
+                    try {
+                        batch_partitions = est.getAllPartitions(qt, base_partition);
+                        if (batch_partitions.size() > 1 || batch_partitions.contains(base_partition) == false) {
+                            partitions_touched_per_batch.addAll(batch_partitions);
+                            
+                            // increment # of multi-partition batches from the base partition
+                            multipartition_batches.put(base_partition);
                     	}
+                        
+                        // Always keep track of which partitions this txn touched
+                        all_partitions.addAll(batch_partitions);
+                        
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
                     }
                 }
-                txn_partitions.getPartitions().add(total_query_partitions);
+                
+                // Add one to each partition that this txn touched
+                heat_histogram.putAll(all_partitions);
+                	
+            	// calculate which other partitions were touched by the multi partition transaction
+                for (int p : partitions_touched_per_batch) {
+                	if (p != base_partition) affinity[base_partition][p]++;                    		
+                } // FOR
             }
-            al_txn_partitions.add(txn_partitions);
-        }
+        } // FOR
         
 
         StringBuilder sb = new StringBuilder();
+        
+        // CLUSTER INFO
         sb.append(String.format("%d %d %d %d\n", total_num_hosts, sites_per_host, total_num_partitions, memory_per_host));
-        sb.append(StringUtil.join(" ", penalties) + "\n");
+        
+        // PENALTIES
+        for (int i = 0; i < penalties.length; i++) {
+            if (i > 0) sb.append(" ");
+            sb.append(penalties[i]);
+        } // FOR
+        sb.append("\n");
         
         // HEAT
-        for (int hist_value : hist.values()) {
+        for (int partition : heat_histogram.values()) {
         	//LOG.info(hist_value  + ": " + hist.get(hist_value));
-    		sb.append(((hist.get(hist_value) * 1.0) / total_num_transactions) + " " + partition_size + "\n");
-        }
+            double partition_heat = heat_histogram.get(partition, 0) / (double)total_num_transactions;
+            // FIXME long partition_size = ??;
+    		sb.append(String.format("%.6f %d\n", partition_heat, partition_size));
+        } // FOR
         
         // AFFINITY
-        for (int i = 0; i < output_matrix.length; i++) {
-            for (int j = 0; j < output_matrix.length; j++) {
-                double aff = output_matrix[i][j] * 1.0 / num_partition_batches[i];
+        for (int i = 0; i < affinity.length; i++) {
+            for (int j = 0; j < affinity.length; j++) {
+                double aff = 1.0 - (affinity[i][j] / multipartition_batches.get(i, 0));
                 LOG.debug(String.format("%d -> %d: %.3f", i, j, aff));
                 sb.append(aff).append(" ");
             } // FOR
@@ -150,26 +151,6 @@ public class TransformTransactionTraces {
         } // FOR
 
         return (sb.toString());
-    }
-    
-    // takes in a list of query traces for a given batch 
-    // returns: true if the batch is multi-partitioned, false otherwise
-    public static boolean isMultiPartitionBatch(List<QueryTrace> traces, PartitionEstimator est, int base_partition) {
-        for (QueryTrace qt : traces) {
-        	Set<Integer> batch_partitions;
-			try {
-				batch_partitions = est.getAllPartitions(qt, base_partition);
-	        	for (int partition : batch_partitions) {
-	        		if (partition != base_partition) {
-	        			return true;
-	        		}
-	        	}                    		        	
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-        }
-        return false;
     }
 
     public static void main(String[] vargs) throws Exception {
@@ -181,7 +162,7 @@ public class TransformTransactionTraces {
                 ArgumentsParser.PARAM_SIMULATOR_HOST_MEMORY
             );
 
-        long memory_per_host = args.getLongParam("simulator.host.memory");
+        long memory_per_host = args.getLongParam(ArgumentsParser.PARAM_SIMULATOR_HOST_MEMORY);
         String output_path = args.getOptParam(0);
         double penalties[] = {
             DEFAULT_DIFFERENT_PARTITION_PENALTY,
@@ -192,6 +173,7 @@ public class TransformTransactionTraces {
         PartitionEstimator p_estimator = new PartitionEstimator(args.catalog_db);
         MemoryEstimator estimator = new MemoryEstimator(args.stats, p_estimator.getHasher());
         long partition_size = (estimator.estimateTotalSize(args.catalog_db) / CatalogUtil.getNumberOfPartitions(args.catalog));
+        partition_size /= 1048576; // 1 MB
         
         String contents = transform(args.workload.getTransactions(), p_estimator, args.catalog_db, memory_per_host, partition_size, penalties);
         
