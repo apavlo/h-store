e8e0a188e7969089178302548a04eb8f5d97b524
Andy Pavlo <pavlo@cs.brown.edu>
2011-10-05 16:05:11 +0000

    New RemoteTransactionInitCallback to work with the new TransactionQueue stuff. This will allow us to acknowledge partitions are ready to execute a dtxn and then only send back message once all of the partitions are ready to go.
    git-svn-id: https://database.cs.brown.edu/svn/hstore/branches/newdtxn-branch@2378 7a8f3d5b-8c5f-44cb-9125-2414a256df87

--- src/frontend/edu/mit/hstore/callbacks/BlockingCallback.java
+++ src/frontend/edu/mit/hstore/callbacks/BlockingCallback.java
@@ -1,17 +1,30 @@
 package edu.mit.hstore.callbacks;
 
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import com.google.protobuf.GeneratedMessage;
 import com.google.protobuf.RpcCallback;
 
+import edu.brown.hstore.Hstore;
 import edu.brown.utils.Poolable;
 
-public abstract class BlockingCallback<T, U extends GeneratedMessage> implements RpcCallback<U>, Poolable {
+/**
+ * 
+ * @param <T> The message type of the original RpcCallback
+ * @param <U> The message type that we will accumulate before invoking the original RpcCallback
+ */
+public abstract class BlockingCallback<T, U> implements RpcCallback<U>, Poolable {
     
     private final AtomicInteger counter = new AtomicInteger(0);
     private RpcCallback<T> orig_callback;
 
+    /**
+     * We'll flip this flag if one of our partitions replies with an
+     * unexpected abort. This ensures that we only send out the ABORT
+     * to all the HStoreSites once. 
+     */
+    private final AtomicBoolean aborted = new AtomicBoolean(false);
+    
     /**
      * Default Constructor
      */
@@ -19,7 +32,21 @@ public abstract class BlockingCallback<T, U extends GeneratedMessage> implements
         // Nothing!
     }
     
-    public abstract void unblockCallback();
+    /**
+     * This method is invoked once all of the T messages are recieved 
+     */
+    protected abstract void unblockCallback();
+    
+    /**
+     * 
+     */
+    protected abstract void abortCallback(Hstore.Status status);
+    
+    /**
+     * 
+     */
+    protected abstract void finishImpl();
+    
     
     @Override
     public void run(U parameter) {
@@ -28,6 +55,18 @@ public abstract class BlockingCallback<T, U extends GeneratedMessage> implements
         if (this.getCounter().decrementAndGet() == 0) {
             this.unblockCallback();
         }
+    }
+    
+    public final void abort(Hstore.Status status) {
+        // If this is the first response that told us to abort, then we'll
+        // send the abort message out 
+        if (this.aborted.compareAndSet(false, true)) {
+            this.abortCallback(status);
+        }
+    }
+    
+    public boolean isAborted() {
+        return (this.aborted.get());
     }
     
     protected final void init(int counter_val, RpcCallback<T> orig_callback) {
@@ -42,7 +81,9 @@ public abstract class BlockingCallback<T, U extends GeneratedMessage> implements
     
     @Override
     public void finish() {
+        this.aborted.set(false);
         this.orig_callback = null;
+        this.finishImpl();
     }
     
     public AtomicInteger getCounter() {
