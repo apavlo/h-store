25059d4d42e85139908637e71af7ffaac7770a2e
Andy Pavlo <pavlo@cs.brown.edu>
2011-10-07 15:35:31 +0000

    Skeleton code for TransactionQueue test cases
    git-svn-id: https://database.cs.brown.edu/svn/hstore/branches/newdtxn-branch@2383 7a8f3d5b-8c5f-44cb-9125-2414a256df87

---
+++ tests/frontend/edu/mit/hstore/util/TestTransactionQueue.java
@@ -0,0 +1,23 @@
+package edu.mit.hstore.util;
+
+import org.junit.Test;
+
+import edu.brown.BaseTestCase;
+import edu.brown.utils.ProjectType;
+
+public class TestTransactionQueue extends BaseTestCase {
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp(ProjectType.TPCC);
+    }
+    
+    /**
+     * testSampleTest
+     */
+    @Test
+    public void testSampleTest() throws Exception {
+        
+    }
+    
+}
