850d463f1bcd3d1288e2cb756801bdddaa21abd0
Andy Pavlo <pavlo@cs.brown.edu>
2011-11-11 22:04:14 +0000

    Finally moved all of the ProtocolBuffer generated files into the source tree. Now we no longer need to call protorpc everytime to build this thing...
    git-svn-id: https://database.cs.brown.edu/svn/hstore/branches/newdtxn-branch@2454 7a8f3d5b-8c5f-44cb-9125-2414a256df87

--- src/frontend/edu/mit/hstore/dtxn/LocalTransaction.java
+++ src/frontend/edu/mit/hstore/dtxn/LocalTransaction.java
@@ -25,7 +25,17 @@
  ***************************************************************************/
 package edu.mit.hstore.dtxn;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
 import java.util.Map.Entry;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CountDownLatch;
