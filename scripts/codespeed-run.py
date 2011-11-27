aec0799edb7d1b85ca9605a9c38ca0810df06112
Andy Pavlo <pavlo@cs.brown.edu>
2011-11-27 00:47:54 +0000

    New CodeSpeed testing script. Fixed it up so that we can execute different cluster configurations. This will allow us to upload results daily as we improve the system. Added the ability for the BenchmarkController to report min/max transaction per seconds
    git-svn-id: https://database.cs.brown.edu/svn/hstore/branches/newdtxn-branch@2492 7a8f3d5b-8c5f-44cb-9125-2414a256df87

--- scripts/codespeed-run.py
+++ scripts/codespeed-run.py
@@ -29,15 +29,15 @@
 # -----------------------------------------------------------------------
 from __future__ import with_statement
 
-'''python executeBenchmarks_by_time.py --endDate=10-25-2011 --startDate=10-25-2011 --url='http://localhost:9000/' --svnRepo='https://database.cs.brown.edu/svn/hstore/trunk/' --svnDir=./dyd/test --environment=hstore_test --executable=myrmidon --branch=default --project=H-store'''
-
 import os
 import sys
 import time
-import datetime
 import pysvn
 import re
+import shutil
+from datetime import datetime, timedelta
 from subprocess import Popen, PIPE, STDOUT
+from pprint import pprint
 
 import logging
 
@@ -65,177 +65,298 @@ CODESPEED_PROJECT = "H-Store"
 CODESPEED_ENVIRONMENT = "default"
 CODESPEED_EXECUTABLE = "default"
 
-RESULT_REGEX = re.compile("Transactions per second:[\s]+([\d]+\.[\d]+)", re.IGNORECASE)
+SVN_REPO_URL = "https://database.cs.brown.edu/svn/hstore/"
+SVN_FILE_URL = "https://database.cs.brown.edu/svn/hstore-files/"
+
+SITES_PER_HOST = 1
+PARTITIONS_PER_SITE = 7
+DATE_FORMAT = "%Y-%m-%d"
+
+RESULT_REGEX = re.compile("Transactions per second:[\s]+([\d]+\.[\d]+)[\s]*(?:\[min:([\d\.]+) \/ max:([\d\.]+)\])?", re.IGNORECASE)
+FAILED_REGEX = re.compile("BUILD FAILED")
+
+## ==============================================
+## makeClusterConfiguration
+## ==============================================
+def makeClusterConfiguration(hostnames, partitions, sites_per_host, partitions_per_site):
+    assert type(hostnames) == list
+    hosts = [ ]
+    host_id = 0
+    site_id = 0
+    partition_id = 0
+    
+    ## HStore Sites
+    LOG.debug("Partitions Per Site: %d" % partitions_per_site)
+    site_hosts = set()
+    for host in hostnames:
+        site_hosts.add(host)
+        for i in range(sites_per_host):
+            firstPartition = partition_id
+            lastPartition = min(partitions, firstPartition + partitions_per_site)-1
+            host = "%s:%d:%d" % (host, site_id, firstPartition)
+            if firstPartition != lastPartition:
+                host += "-%d" % lastPartition
+            partition_id += partitions_per_site
+            site_id += 1
+            hosts.append(host)
+            if lastPartition+1 == partitions: break
+        ## FOR (SITES)
+        if lastPartition+1 == partitions: break
+    ## FOR
+    assert len(hosts) > 0
+    return hosts
+## DEF
 
 ## ==============================================
 ## executeBenchmark
 ## ==============================================
-def executeBenchmark(benchmark, cid, date, branch, ):
-    LOG.info("Preparing benchmark '%s' using SVN revision r%d" % (benchmark, cid))
-    #overide the temp_dir when preparing benchmarks.
-    prep = 'ant hstore-prepare -Dproject=' + benchmark
-    p = Popen(prep, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
-    output = p.stdout.read()
+def executeBenchmark(svnWorkDir, revision, benchmark, args):
+    LOG.info("Preparing benchmark '%s' using SVN revision r%d" % (benchmark, revision))
 
-    if output.find('BUILD FAILED') != -1:
-        print output
-        raise Exception("Unexpected error when executing '%s'" % benchmark)
-
-    LOG.info("Executing benchmark '%s' using SVN revision r%d" % (benchmark, cid))
-    run = 'ant hstore-benchmark -Dproject=%s' % benchmark
-    p = Popen(run, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
-    output = p.stdout.read()
-
-    if output.find('BUILD FAILED') != -1:
-        LOG.warn(output)
-        raise Exception("Unexpected error")
-
-    match = RESULT_REGEX.search(output)    
-    if match == None:
-        LOG.warn(output)
-        raise Exception("Unable to find transactions per second result")
+    cmdArgs = " ".join(map(lambda x: "-D%s=%s" % (x, args[x]), args.keys()))
     
-    return float(match.group(1))
+    try:
+        os.chdir(svnWorkDir)
+        cmd = "ant hstore-prepare -Dproject=%s %s" % (benchmark, cmdArgs)
+        p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+        output = p.stdout.read()
+
+        if output.find('BUILD FAILED') != -1:
+            LOG.warn("Failed to execute command:\n" + cmd)
+            LOG.warn("Invalid Output::\n" + output)
+            raise Exception("Unexpected error when executing '%s'" % benchmark)
+
+        LOG.info("Executing benchmark '%s' using SVN revision r%d" % (benchmark, revision))
+        cmd = "ant hstore-benchmark -Dproject=%s %s" % (benchmark, cmdArgs)
+        p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+        output = p.stdout.read()
+
+        if output.find('BUILD FAILED') != -1:
+            LOG.warn("Failed to execute command:\n" + cmd)
+            LOG.warn("Invalid Output::\n" + output)
+            raise Exception("Unexpected error")
+
+        match = RESULT_REGEX.search(output)
+        if match == None:
+            LOG.warn(output)
+            raise Exception("Unable to find transactions per second result")
+        
+        result = match.group(1)
+        min_result = match.group(2)
+        max_result = match.group(3)
+    
+        return (result, min_result, max_result)
+    finally:
+        os.chdir("..")
 ## DEF
 
 ## ==============================================
 ## checkoutHStore
 ## ==============================================
-def checkoutHStore(startDate, endDate, svnRepo, svnDir, url, benchmark, environment, executable, branch, project):
-    #print 'startDate:'      , info['startDate']
-    #print 'endDate:'        , info['endDate']
-    #print 'url:'            , info['url']
-    #print 'svnRepo:'        , info['svnRepo']
-    #print 'svnDir:'         , info['svnDir']
-    #print 'environment:'    , info['environment']
-    #print 'executable:'     , info['executable']
-    #print 'branch:'         , info['branch']
-    #print 'project:'        , info['project']
+def checkoutHStore(svnRepo, svnDir, rev):
+    LOG.info("Checking out '%s' at revision r%d into '%s'" % (os.path.basename(svnRepo), rev, svnDir))
+    if not os.path.exists(svnDir):
+        client.checkout(svnRepo, svnDir, \
+                        revision=pysvn.Revision(pysvn.opt_revision_kind.number, rev), \
+                        ignore_externals=True)
+    ## IF
+    os.chdir(svnDir)
     
-    client = pysvn.Client();
-    err_cnt = 0
-    err = ('Benchmark', 'Version', 'Time')
-    while startDate <= endDate :
-        tmp = startDate + datetime.timedelta(days=1)
-        epoch = float(time.mktime(time.strptime(str(tmp), "%Y-%m-%d")))
-        info = client.info2(svnRepo, revision=pysvn.Revision(pysvn.opt_revision_kind.date, epoch), recurse=False)
-        cid = info[0][1]["rev"].number 
-
-        LOG.info("Checking out H-Store at SVN revision r%d (%s)" % (cid, startDate))
-        client.checkout(svnRepo, svnDir, revision=pysvn.Revision(pysvn.opt_revision_kind.date, epoch), ignore_externals=True)
-        os.chdir(svnDir)
-        
-        LOG.info("Building H-Store at SVN reversion r%d (%s)" % (cid, startDate))
+    LOG.info("Building '%s' at revision r%d in '%s'" % (os.path.basename(svnRepo), rev, svnDir))
+    try:
         p = Popen('ant build', shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
         output = p.stdout.read()
-        if output.find('BUILD FAILED') != -1:
+        if FAILED_REGEX.search(output):
             print output
-            LOG.error("Unexpected error when building SVN revision r%d (%s)" % (cid, startDate))
-            continue
-        
-        result = None
-        try:
-            result = executeBenchmark(benchmark, cid, startDate, branch, project, executable, environment, url)
-        except:
-            err_cnt = err_cnt + 1;
-            err = err, (benchmark, cid, startDate)
-        
-        if result != None:
-            LOG.info("Uploading '%s' results to CODESPEED at %s" % (benchmark, url))
-            result = codespeed.Result(
-                commitid=cid,
-                branch=branch,
-                project=project,
-                executable=executable,
-                benchmark=benchmark,
-                environment=environment,
-                result_value=result_value,
-                revision_date=date,
-                result_date=date
-            )
-            result.upload(data)
-        ## IF
-        
+            raise Exception("Unexpected error when building SVN revision r%d (%s)" % (rev, startDate))
+    finally:
         os.chdir("..")
-        startDate = tmp
-    ## WHILE
-    return err_cnt, err
+    
+    return True
 ## DEF
 
 ## ==============================================
 ## main
 ## ==============================================
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Running H-Store benchmark.')
+    aparser = argparse.ArgumentParser(description='Running H-Store benchmark.')
     
     ## H-STORE OPTIONS
-    parser.add_argument('benchmark', choices=[ 'tpcc', 'tm1', 'auctionmark', 'locality', 'airline' ],
+    aparser.add_argument('hosts', nargs='+', default=['localhost'],
+                         help='HStoreSite hosts')
+    aparser.add_argument('--benchmark', nargs='+', choices=[ 'tpcc', 'tm1', 'auctionmark', 'locality', 'airline' ],
                          help='Target benchmark')
+    aparser.add_argument('--partitions', nargs='+', default=[2,4,8],
+                         help='Total number of partitions in cluster')
+    aparser.add_argument('--sites-per-host', default=SITES_PER_HOST,
+                         help='Number of sites per host')
+    aparser.add_argument('--partitions-per-site', default=PARTITIONS_PER_SITE,
+                         help='Number of partitions per site')
                          
     ## SUBVERSION OPTIONS
-    parser.add_argument('--endDate',
+    aparser.add_argument('--endDate',
                         help='The end date of running H-Store subversions! Eg: 12-31-2010.')
-    parser.add_argument('--startDate',
+    aparser.add_argument('--startDate',
                         help='The start date of running H-Store subversions! Eg: 12-31-2010. \n' + 
                             'If there is no input of the start date, ' + 
                             'it will automatically run the versions of last 30 days.')
-    parser.add_argument('--svnRepo', default = 'https://database.cs.brown.edu/svn/hstore/trunk/',
+    aparser.add_argument('--svn-repo', default=SVN_REPO_URL,
                         help='The public repository where to download H-Store.')
-    #parser.add_argument('--svnDir', default = './svnDir', 
-                        #help='The svn file directory and remember that do not forget to add the "./" in the beginning.')
+    aparser.add_argument('--svn-branch', default="trunk",
+                        help='The branch you should create for CODESPEED project.')
     
     ## CODESPEED OPTIONS
-    parser.add_argument('--url', help='The CODESPEED url.')
-    parser.add_argument('--project', default=CODESPEED_PROJECT,
-                        help='The name you should create for CODESPEED project.')
-    parser.add_argument('--environment', default=CODESPEED_ENVIRONMENT,
-                        help='The environment you created earlier in the CODESPEED admin website.')
-    parser.add_argument('--executable', default=CODESPEED_EXECUTABLE,
-                        help='The executable you should create for CODESPEED project.')
-    parser.add_argument('--branch', default=None,
-                        help='The branch you should create for CODESPEED project.')
-    args = parser.parse_args()
+    aparser.add_argument('--codespeed-url',
+                         help='The URL of the CODESPEED site to post benchmark results to.')
+    aparser.add_argument('--codespeed-project', default=CODESPEED_PROJECT,
+                         help='The name of the CODESPEED project used for these results.')
+    aparser.add_argument('--codespeed-environment', default=CODESPEED_ENVIRONMENT,
+                         help='The name of the CODESPEED environment used for these results.')
 
+    ## MISCELLANEOUS OPTIONS
+    aparser.add_argument('--workDir', default='/tmp', 
+                         help='The working directory to use for building and executing H-Store.')
+    aparser.add_argument('--clean', action='store_true',
+                         help='Remove SVN checkouts after executing benchmarks.')
+    aparser.add_argument('--overwrite', action='store_true',
+                         help='Remove existing SVN checkouts.')
+    aparser.add_argument('--debug', action='store_true',
+                         help='Enable debug log messages')
+    args = vars(aparser.parse_args())
+    
+    
     ## ----------------------------------------------
     
-    endDate         = args.endDate
-    startDate       = args.startDate
-    url             = args.url
-    svnRepo         = args.svnRepo
-    svnDir          = args.svnDir
-    environment     = args.environment
-    executable      = args.executable
-    branch          = args.branch
-    project         = args.project
+    if args['debug']: logging.getLogger().setLevel(logging.DEBUG)
+    
+    svnRepo          = args["svn_repo"]
+    svnBranch        = args["svn_branch"]
+    workDir          = args["workDir"]
+    codespeedUrl     = args["codespeed_url"]
+    codespeedProject = args["codespeed_project"]
+    codespeedEnv     = args["codespeed_environment"]
 
-    endDate = time.strptime(endDate, "%m-%d-%Y")
-    endDate = datetime.date(endDate[0],endDate[1],endDate[2])
-
-    if endDate == None:
-        start = endDate - datetime.timedelta(days=30)
+    if not codespeedUrl:
+        raise Exception("Missing CODESPEED URL")
+    if not codespeedProject:
+        raise Exception("Missing CODESPEED Project")
+    if not codespeedEnv:
+        raise Exception("Missing CODESPEED Environment")
+    
+    if svnBranch == "trunk":
+        svnRepo = os.path.join(SVN_REPO_URL, svnBranch)
     else:
-        startDate = time.strptime(startDate, "%m-%d-%Y")
-        startDate = datetime.date(startDate[0],startDate[1],startDate[2])
+        svnRepo = os.path.join(SVN_REPO_URL, "branches", svnBranch)
+    LOG.debug("Target SVN Repository: " + svnRepo)
+    
+    ## ----------------------------------------------
+    
+    if "endDate" in args and args["endDate"]:
+        endDate = datetime.strptime(args["endDate"], DATE_FORMAT)
+    else:
+        now = datetime.now()
+        endDate = datetime(now.year, now.month, now.day)
+    endDate = endDate + timedelta(days=1) - timedelta(seconds=1)
+    LOG.debug("endDate: %s" % endDate)
+     
+    if "startDate" in args and args["startDate"]:
+        startDate = datetime.strptime(args["startDate"], DATE_FORMAT)
+    else:
+        startDate = endDate - timedelta(days=1)
+    LOG.debug("startDate: %s" % startDate)
 
     if startDate > endDate:
         raise Exception("The start date must be before the end date")
     
+    startRev = pysvn.Revision(pysvn.opt_revision_kind.date, time.mktime(startDate.timetuple()))
+    endRev = pysvn.Revision(pysvn.opt_revision_kind.date, time.mktime(endDate.timetuple()))
+    
     client = pysvn.Client()
     try:
-        info = client.info2(svnRepo, revision=pysvn.Revision(pysvn.opt_revision_kind.date, time.time()), recurse=False)
+        info = client.log(svnRepo, \
+                          revision_start=startRev, \
+                          revision_end=endRev )
         LOG.debug("SVN respository '%s' exists" % svnRepo)
     except pysvn.ClientError, ex:
         if 'non-existent' in ex.args[0]:
             raise Exception("SVN repository %s does not exist" % svnRepo)
         else:
             raise Exception("Unexpected error: %s" % ex.args[0])
+    if not info:
+        LOG.warn("No revisions were found in %s from %s to %s" % (svnRepo, startDate, endDate))
+        sys.exit(0)
+    assert info
+    #print pprint(info[-1].items())
+    #print pprint(info)
+    #sys.exit(0)
 
-    cnt, err = checkoutHStore(startDate, endDate, \
-                              svnRepo, svnDir, \
-                              url, environment, executable, branch, project)
-    if cnt != 0:
-        print 'There were (%d) errors while running H-Store from (%s) to (%s)' % (cnt, str(start), str(end))
-        print err
-        for x in err:
-            print x
+    ## For each revision, check it out of the repository and execute all
+    ## the benchmarks that we need to
+    created = set()
+    try:
+        for svnLog in info:
+            revision = svnLog["revision"].number
+            revisionDate = datetime.fromtimestamp(svnLog["date"])
+            
+            svnWorkDir = os.path.join(workDir, "%s-r%d" % (svnBranch, revision))
+            if os.path.exists(svnWorkDir) and args["overwrite"]:
+                LOG.info("Removing existing SVN checkout '%s'" % svnWorkDir)
+                shutil.rmtree(svnWorkDir)
+            created.add(svnWorkDir)
+            checkoutHStore(svnRepo, svnWorkDir, revision)
+            
+            for num_partitions in args['partitions']:
+                codespeedExec = "%02d-partitions" % num_partitions
+                cluster = makeClusterConfiguration(args['hosts'], num_partitions, SITES_PER_HOST, PARTITIONS_PER_SITE)
+                cmdArgs = {
+                    "hosts":                        '"' + (";".join(cluster)) + '"',
+                    "client.processesperclient":    500,
+                    "client.txnrate":               1000,
+                    "client.blocking":              True,
+                    "client.blocking_concurrent":   1,
+                }
+                
+                os.chdir(workDir)
+                for benchmark in args['benchmark']:
+                    # Get PartitionPlan from hstore-files
+                    pplan = "%s.%s.pplan" % (benchmark, "lns")
+                    
+                    if not os.path.exists(pplan):
+                        pplanURL = os.path.join(SVN_FILE_URL, "designplans/vldb-aug2011/", pplan)
+                        LOG.info("Downloading PartitionPlan '%s'" % pplanURL)
+                        with open(pplan, "w") as fd:
+                            contents = client.cat(pplanURL)
+                            fd.write(contents)
+                        ## WITH
+                    ## IF
+                    cmdArgs["partitionplan"] = os.path.realpath(pplan)
+                    
+                    try:
+                        result, min_result, max_result = executeBenchmark(svnWorkDir, revision, benchmark, cmdArgs)
+                        assert result
+
+                        LOG.info("Uploading %s/%s results to CODESPEED at %s" % (codespeedExec, benchmark, codespeedUrl))
+                        result = codespeed.Result(
+                            commitid=revision,
+                            branch=svnBranch,
+                            benchmark=benchmark,
+                            project=codespeedProject,
+                            executable=codespeedExec,
+                            environment=codespeedEnv,
+                            result_value=result,
+                            revision_date=revisionDate,
+                            result_date=datetime.now(),
+                            min_result=min_result,
+                            max_result=max_result
+                        )
+                        result.upload(codespeedUrl)
+                        
+                    except Exception, ex:
+                        #LOG.error("Unexpected error: %s" % ex.args[0])
+                        raise
+                ## FOR (benchmark)
+            ## FOR (partitions)
+        # FOR (revisions)
+    finally:
+        if args["clean"]: map(shutil.rmtree, created)
 ## MAIN
