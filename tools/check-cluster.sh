2fbc39aceede3456c6f160cc34ab3cf3a3db806f
Andy Pavlo <pavlo@cs.brown.edu>
2012-01-04 20:35:42 -0500

    Removed some old VoltDB files. Added .gitignore Fixed build output directory

--- tools/check-cluster.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for i in `echo 3a 3b 3c 3d 3e 4a 4b 4c`; do
-    echo $i
-    ssh volt$i ps -ef | grep java && exit
-done
-
--- tools/start-hudson.sh
+++
@@ -1,2 +0,0 @@
-cat /var/log/hudson >> /var/log/hudson.0
-java -Duser.timezone="America/New_York" -jar hudson.war &> /var/log/hudson &
