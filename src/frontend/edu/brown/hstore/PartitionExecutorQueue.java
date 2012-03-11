6ad755ad892871362ccf6470f5fe76f219a8534c
Andy Pavlo <pavlo@cs.brown.edu>
2012-03-11 19:28:35 -0400

    Simple fix to let systems procedures go first in the PartitionExecutorQueue. I don't know why I did this. Also moved out some code from HStoreSite.procedureInvocation() into HStoreSite.transactionRedirect()

--- src/frontend/edu/brown/hstore/PartitionExecutorQueue.java
+++ src/frontend/edu/brown/hstore/PartitionExecutorQueue.java
@@ -46,21 +46,30 @@ public class PartitionExecutorQueue extends PriorityBlockingQueue<TransactionInf
             assert(msg0 != null);
             assert(msg1 != null);
             
+            // (1) SysProcs always go first
+            if (msg0.isSysProc() != msg1.isSysProc()) {
+                if (msg0.isSysProc()) return (-1);
+                else return (1);
+            }
+            
+            // (2) If they're the same message type, go by their txnIds
             Class<? extends TransactionInfoBaseMessage> class0 = msg0.getClass();
             Class<? extends TransactionInfoBaseMessage> class1 = msg1.getClass();
-            
             if (class0.equals(class1)) return (msg0.getTxnId().compareTo(msg1.getTxnId()));
 
+            // (3) Otherwise, always let the FinishTaskMessage go first
             boolean isFinish0 = class0.equals(FinishTaskMessage.class);
             boolean isFinish1 = class1.equals(FinishTaskMessage.class);
             if (isFinish0 && !isFinish1) return (-1);
             else if (!isFinish0 && isFinish1) return (1);
             
+            // (4) Then let a FragmentTaskMessage go before anything else
             boolean isWork0 = class0.equals(FragmentTaskMessage.class);
             boolean isWork1 = class1.equals(FragmentTaskMessage.class);
             if (isWork0 && !isWork1) return (-1);
             else if (!isWork0 && isWork1) return (1);
             
+            // (5) They must be the same!
             assert(false) : String.format("%s <-> %s", class0, class1);
             return 0;
         }
