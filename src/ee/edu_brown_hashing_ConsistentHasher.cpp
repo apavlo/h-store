fb56141cb4e653527a2ee071f55c8f62cb55bfe0
Yang Zou <yangzou@me.com>
2012-03-27 14:29:05 -0400

    link error

--- src/ee/edu_brown_hashing_ConsistentHasher.cpp
+++ src/ee/edu_brown_hashing_ConsistentHasher.cpp
@@ -1,5 +1,7 @@
 #include "edu_brown_hashing_ConsistentHasher.h"
-#include "conhash.h"
+#include "../../third_party/cpp/libconhash/conhash.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 JNIEXPORT jlong JNICALL Java_edu_brown_hashing_ConsistentHasher_nativeCreate
 (JNIEnv *env, jobject obj, jint num_partitions){
@@ -15,21 +17,25 @@ JNIEXPORT jlong JNICALL Java_edu_brown_hashing_ConsistentHasher_nativeCreate
  			conhash_add_node(conhash_ &g_nodes[i]);
  		}
 	}
-	return conhash;
+	return reinterpret_cast<jlong>(conhash);
+}
 
 JNIEXPORT jint JNICALL Java_edu_brown_hashing_ConsistentHasher_nativeDestroy
-  (JNIEnv *env, jobject obj, jlong conhash){
+  (JNIEnv *env, jobject obj, jlong hash_pointer){
+  	struct conhash_s *conhash = reinterpret_cast<struct conhash_s *>(hash_pointer);
   	conhash_fini(conhash);
+ 	return 0; 
   }
 
 
 JNIEXPORT jint JNICALL Java_edu_brown_hashing_ConsistentHasher_nativeHashinate
-  (JNIEnv *env, jobject obj, jlong conhash, jint value){
+  (JNIEnv *env, jobject obj, jlong hash_pointer, jint value){
   	char str[128];
+  	struct conhash_s *conhash = reinterpret_cast<struct conhash_s *>(hash_pointer);
   	sprintf(str, "%d", value);
  	const struct node_s *node;
  	node = conhash_lookup(conhash, str);
- 	return node->iden;
+ 	return atoi(node->iden);
   }
 
 
