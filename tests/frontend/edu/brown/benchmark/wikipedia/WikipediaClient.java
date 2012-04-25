ab5470a9bc2a4c419dc07d8ca81a9c983055298f
Xin Jia <jiaxincs@gmail.com>
2012-04-25 15:22:00 -0400

    Add back Transaction type for wikipedia, generate parameters for by invoke generateParams()

--- tests/frontend/edu/brown/benchmark/wikipedia/WikipediaClient.java
+++ tests/frontend/edu/brown/benchmark/wikipedia/WikipediaClient.java
@@ -27,6 +27,8 @@ import java.util.HashMap;
 import java.util.Random;
 
 import org.apache.log4j.Logger;
+import org.voltdb.VoltProcedure;
+import org.voltdb.VoltProcedure.VoltAbortException;
 import org.voltdb.VoltTable;
 import org.voltdb.catalog.Procedure;
 import org.voltdb.client.Client;
@@ -37,7 +39,6 @@ import org.voltdb.client.ProcedureCallback;
 import com.oltpbenchmark.benchmarks.wikipedia.util.WikipediaOperation;
 
 import edu.brown.benchmark.BenchmarkComponent;
-import edu.brown.benchmark.tm1.TM1Client.Transaction;
 import edu.brown.benchmark.wikipedia.procedures.AddWatchList;
 import edu.brown.benchmark.wikipedia.procedures.GetPageAnonymous;
 import edu.brown.benchmark.wikipedia.procedures.GetPageAuthenticated;
@@ -45,7 +46,9 @@ import edu.brown.benchmark.wikipedia.procedures.GetPagesInfo;
 import edu.brown.benchmark.wikipedia.procedures.RemoveWatchList;
 import edu.brown.benchmark.wikipedia.procedures.UpdatePage;
 import edu.brown.hstore.Hstoreservice.Status;
+import edu.brown.rand.RandomDistribution.Flat;
 import edu.brown.rand.RandomDistribution.FlatHistogram;
+import edu.brown.rand.RandomDistribution.Zipf;
 import edu.brown.statistics.Histogram;
 
 public class WikipediaClient extends BenchmarkComponent {
@@ -56,14 +59,45 @@ public class WikipediaClient extends BenchmarkComponent {
 //	final int num_users;
 	private Random randGenerator = new Random();
 	private int nextRevId;
+	public HashMap<Long, String[]> m_titleMap = new HashMap<Long, String[]>();
 	
+	
+	// TODO:(xin)
+	// Change from hashMap to array in order to make it faster
+	public Object [] m_titleArr; 
+	
+	
+	 
+
+    /**
+     * Set of transactions structs with their appropriate parameters
+     */
+    public static enum Transaction {
+        ADD_WATCHLIST("Add watch list", WikipediaConstants.FREQUENCY_ADD_WATCHLIST),
+        GET_PAGE_ANONYMOUS("Get page anonymous", WikipediaConstants.FREQUENCY_GET_PAGE_ANONYMOUS),
+        GET_PAGE_AUTHENTICATED("Get page authenticated", WikipediaConstants.FREQUENCY_GET_PAGE_AUTHENTICATED),
+        REMOVE_WATCHLIST("Remove watchlist", WikipediaConstants.FREQUENCY_REMOVE_WATCHLIST),
+        UPDATE_PAGE("Update page", WikipediaConstants.FREQUENCY_UPDATE_PAGE);
+
+        /**
+         * Constructor
+         */
+        private Transaction (String displayName, int weight) {
+            this.displayName = displayName;
+            this.callName = displayName.replace(" ", "");
+            this.weight = weight;
+        }
+        public final String displayName;
+        public final String callName;
+        public final int weight; // probability (in terms of percentage) the
+        // transaction gets executed
+    }
 	
     /**
      * Callback Class
      * @author xin
      *
      */
-    
     private class WikipediaCallback implements ProcedureCallback {
         private final int idx;
  
@@ -84,7 +118,7 @@ public class WikipediaClient extends BenchmarkComponent {
      * Data Members
      */
 
-    // Storing the ordinals of transaction per tm1 probability distribution
+    // Storing the ordinals of transaction per wikipedia probability distribution
     private final FlatHistogram<Transaction> txnWeights;
 //    private final int[] txnWeights = new int[100];
 
@@ -160,30 +194,44 @@ public class WikipediaClient extends BenchmarkComponent {
         }
         assert(cr != null);
         assert(cr.getStatus() == Status.OK);
-
+        
+        
+        // TODO(xin):
         // Execute GetArticles stored procedure and get back the list
         // of pageIds and pageTitles.
         // Build a HashMap<Long, String[]> that gives you the pageTitle for a pageId
         // Where first element is namespace, second is title
-
         VoltTable res[] = cr.getResults();
         VoltTable vt = res[0];
-        HashMap<Long, String[]> titleMap = new HashMap<Long, String[]>();
+        
         while (vt.advanceRow()) {
             long page_id = vt.getLong(0);
             long namespace = vt.getLong(1);
             String title = vt.getString(2);
             String data[] = { Long.toString(namespace), title}; 
-            if (!titleMap.containsKey(page_id)) {
-                titleMap.put(page_id, data);
+            if (!m_titleMap.containsKey(page_id)) {
+                m_titleMap.put(page_id, data);
             } else {
                 assert(false):"There should not have duplicate page_ids";
             }
         }
         
+        
+        double m_scalefactor = 1.0;
+        int num_users = (int) Math.round(WikipediaConstants.USERS * m_scalefactor);
+        //int num_pages = (int) Math.round(WikipediaConstants.PAGES * m_scalefactor);
+        
+        Flat z_users = new Flat(this.randGenerator, 1, num_users);
+        Zipf z_pages = new Zipf(this.randGenerator, 1, m_titleMap.size(), WikipediaConstants.USER_ID_SIGMA);
+        
+        
         try {
             while (true) {
-                this.runOnce();
+                // Figure out what page they're going to update
+                int user_id = z_users.nextInt();
+                int page_id = z_pages.nextInt();
+                
+                this.runOnce(user_id,page_id);
                 client.backpressureBarrier();
             } // WHILE
         } catch (Exception ex) {
@@ -191,17 +239,18 @@ public class WikipediaClient extends BenchmarkComponent {
         }
     }
 	
-	 @Override
-	    protected boolean runOnce() throws IOException {
+	 
+	    protected boolean runOnce(int user_id,int page_id) throws IOException {
 	        final Transaction target = this.selectTransaction();
 
 	        this.startComputeTime(target.displayName);
-	        //Object params[] = target.ag.genArgs(10);
+	        Object params[] = this.generateParams(target, user_id, page_id);
+	       
 	        this.stopComputeTime(target.displayName);
-
-	        //boolean ret = this.getClientHandle().callProcedure(this.callbacks[target.ordinal()], target.callName, params);
+	        boolean ret = this.getClientHandle().callProcedure(this.callbacks[target.ordinal()], target.callName, params);
+	        
 	        LOG.debug("Executing txn " + target);
-	        return true;
+	        return ret;
 	    }
  
 	private String generateUserIP() {
@@ -222,116 +271,131 @@ public class WikipediaClient extends BenchmarkComponent {
         return (procNames);
     }
 	
-   
 
-    @Override
-    protected Status executeWork(TransactionType nextTransaction) throws VoltAbortException {
-        WikipediaOperation t = null;
+    protected Object[] generateParams(Transaction txn, int user_id, int page_id) throws VoltAbortException {
         
-        Class<? extends Procedure> procClass = nextTransaction.getProcedureClass();
-        boolean needUser = (procClass.equals(AddWatchList.class) ||
-                            procClass.equals(RemoveWatchList.class) ||
-                            procClass.equals(GetPageAuthenticated.class));    
-        while (t == null) {
-            t = this.generator.nextTransaction();
-            if (needUser && t.userId == 0) {
-                t = null;
-            }
-        } // WHILE
-        assert(t != null);
-        if (t.userId != 0) t.userId = this.randGenerator.nextInt();
-        
+        String data[] = m_titleMap.get(page_id);
+       
         // AddWatchList
-        if (procClass.equals(AddWatchList.class)) {
-            assert(t.userId > 0);
-            addToWatchlist(t.userId, t.nameSpace, t.pageTitle);
+        if (txn.callName.equals(AddWatchList.class.getName())) {
+            
+            Object params[] = {
+                    user_id, 
+                    new Integer(data[0]), 
+                    data[1]
+            };
+            return params;
         }
         // RemoveWatchList
-        else if (procClass.equals(RemoveWatchList.class)) {
-            assert(t.userId > 0);
-            removeFromWatchlist(t.userId, t.nameSpace, t.pageTitle);
+        else if (txn.callName.equals(RemoveWatchList.class.getName())) {
+            Object params[] = {
+                    user_id, 
+                    new Integer(data[0]), 
+                    data[1]
+            };
+            return params;
         }
         // UpdatePage
-        else if (procClass.equals(UpdatePage.class)) {
-            updatePage(this.generateUserIP(), t.userId, t.nameSpace, t.pageTitle);
+        else if (txn.callName.equals(UpdatePage.class.getName())) {
+            Object params[] = {
+                    this.generateUserIP(),
+                    user_id, 
+                    new Integer(data[0]), 
+                    data[1]
+            };
+            return params;
         }
         // GetPageAnonymous
-        else if (procClass.equals(GetPageAnonymous.class)) {
-            getPageAnonymous(true, this.generateUserIP(), t.nameSpace, t.pageTitle);
+        else if (txn.callName.equals(GetPageAnonymous.class.getName())) {
+            Object params[] = {
+                    true,
+                    this.generateUserIP(),
+                    user_id, 
+                    new Integer(data[0]), 
+                    data[1]
+            };
+            return params;
         }
         // GetPageAuthenticated
-        else if (procClass.equals(GetPageAuthenticated.class)) {
-            assert(t.userId > 0);
-            getPageAuthenticated(true, this.generateUserIP(), t.userId, t.nameSpace, t.pageTitle);
+        else if (txn.callName.equals(GetPageAuthenticated.class.getName())) {
+            Object params[] = {
+                    true,
+                    this.generateUserIP(),
+                    user_id, 
+                    new Integer(data[0]), 
+                    data[1]
+            };
+            return params;
         }
         
-        return (Status.OK);
+        assert(false):"Should never come to this point";
+        return null;
     }
     
     
-    /**
-	 * Implements wikipedia selection of last version of an article (with and
-	 * without the user being logged in)
-	 * 
-	 * @parama userIp contains the user's IP address in dotted quad form for
-	 *         IP-based access control
-	 * @param userId
-	 *            the logged in user's identifer. If negative, it is an
-	 *            anonymous access.
-	 * @param nameSpace
-	 * @param pageTitle
-	 * @return article (return a Class containing the information we extracted,
-	 *         useful for the updatePage transaction)
-	 * @throws SQLException
-	 * @throws UnknownHostException
-	 */
-	public VoltTable getPageAnonymous(boolean forSelect, String userIp,
-			                        int nameSpace, String pageTitle) {
-		GetPageAnonymous proc = this.getProcedure(GetPageAnonymous.class);
-        assert (proc != null);
-        return proc.run(forSelect, userIp, nameSpace, pageTitle);
-	}
-
-	public VoltTable getPageAuthenticated(boolean forSelect, String userIp, int userId,
-			                            int nameSpace, String pageTitle) {
-		GetPageAuthenticated proc = this.getProcedure(GetPageAuthenticated.class);
-        assert (proc != null);
-        return proc.run( forSelect, userIp, userId, nameSpace, pageTitle);
-	}
-	
-	public void addToWatchlist(int userId, int nameSpace, String pageTitle) {
-		AddWatchList proc = this.getProcedure(AddWatchList.class);
-        assert (proc != null);
-        proc.run( userId, nameSpace, pageTitle);
-	}
-
-	public void removeFromWatchlist(int userId, int nameSpace, String pageTitle) {
-		RemoveWatchList proc = this.getProcedure(RemoveWatchList.class);
-        assert (proc != null);
-        proc.run( userId, nameSpace, pageTitle);
-	}
-
-	public void updatePage(String userIp, int userId, int nameSpace, String pageTitle) {
-	    VoltTable a = getPageAnonymous(false, userIp, nameSpace, pageTitle);
-		
-		// TODO: If the Article is null, then we want to insert a new page.
-		//       But we don't support that right now.
-		if (a == null) return;
-		
-//		WikipediaBenchmark b = this.getBenchmarkModule();
-//		int revCommentLen = b.commentLength.nextValue().intValue();
-//		String revComment = TextGenerator.randomStr(randGenerator, revCommentLen);
-//		int revMinorEdit = b.minorEdit.nextValue().intValue();
+//    /**
+//	 * Implements wikipedia selection of last version of an article (with and
+//	 * without the user being logged in)
+//	 * 
+//	 * @parama userIp contains the user's IP address in dotted quad form for
+//	 *         IP-based access control
+//	 * @param userId
+//	 *            the logged in user's identifer. If negative, it is an
+//	 *            anonymous access.
+//	 * @param nameSpace
+//	 * @param pageTitle
+//	 * @return article (return a Class containing the information we extracted,
+//	 *         useful for the updatePage transaction)
+//	 * @throws SQLException
+//	 * @throws UnknownHostException
+//	 */
+//	public VoltTable getPageAnonymous(boolean forSelect, String userIp,
+//			                        int nameSpace, String pageTitle) {
+//		GetPageAnonymous proc = this.getProcedure(GetPageAnonymous.class);
+//        assert (proc != null);
+//        return proc.run(forSelect, userIp, nameSpace, pageTitle);
+//	}
+//
+//	public VoltTable getPageAuthenticated(boolean forSelect, String userIp, int userId,
+//			                            int nameSpace, String pageTitle) {
+//		GetPageAuthenticated proc = this.getProcedure(GetPageAuthenticated.class);
+//        assert (proc != null);
+//        return proc.run( forSelect, userIp, userId, nameSpace, pageTitle);
+//	}
+//	
+//	public void addToWatchlist(int userId, int nameSpace, String pageTitle) {
+//		AddWatchList proc = this.getProcedure(AddWatchList.class);
+//        assert (proc != null);
+//        proc.run( userId, nameSpace, pageTitle);
+//	}
+//
+//	public void removeFromWatchlist(int userId, int nameSpace, String pageTitle) {
+//		RemoveWatchList proc = this.getProcedure(RemoveWatchList.class);
+//        assert (proc != null);
+//        proc.run( userId, nameSpace, pageTitle);
+//	}
+//
+//	public void updatePage(String userIp, int userId, int nameSpace, String pageTitle) {
+//	    VoltTable a = getPageAnonymous(false, userIp, nameSpace, pageTitle);
 //		
-//		// Permute the original text of the article
-//		// Important: We have to make sure that we fill in the entire array
-//		char newText[] = b.generateRevisionText(a.oldText.toCharArray());
-
-		UpdatePage proc = this.getProcedure(UpdatePage.class);
-        assert (proc != null);
-//        proc.run(this.nextRevId++, a.pageId, pageTitle, new String(newText),
-//                       nameSpace, userId, userIp, a.userText,
-//                       a.revisionId, revComment, revMinorEdit);
-	}
+//		// TODO: If the Article is null, then we want to insert a new page.
+//		//       But we don't support that right now.
+//		if (a == null) return;
+//		
+////		WikipediaBenchmark b = this.getBenchmarkModule();
+////		int revCommentLen = b.commentLength.nextValue().intValue();
+////		String revComment = TextGenerator.randomStr(randGenerator, revCommentLen);
+////		int revMinorEdit = b.minorEdit.nextValue().intValue();
+////		
+////		// Permute the original text of the article
+////		// Important: We have to make sure that we fill in the entire array
+////		char newText[] = b.generateRevisionText(a.oldText.toCharArray());
+//
+//		UpdatePage proc = this.getProcedure(UpdatePage.class);
+//        assert (proc != null);
+////        proc.run(this.nextRevId++, a.pageId, pageTitle, new String(newText),
+////                       nameSpace, userId, userIp, a.userText,
+////                       a.revisionId, revComment, revMinorEdit);
+//	}
 
 }
