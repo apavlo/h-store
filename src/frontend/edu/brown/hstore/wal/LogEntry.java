4761b9892e6b349d6d96c93d28b039c98f98cd2e
Max Kirsch <mkirsch@cs.brown.edu>
2012-05-02 21:32:14 -0400

    Writing/Reading working with SimpleTest written

--- src/frontend/edu/brown/hstore/wal/LogEntry.java
+++ src/frontend/edu/brown/hstore/wal/LogEntry.java
@@ -10,9 +10,9 @@ import org.voltdb.utils.EstTime;
 
 public class LogEntry implements FastSerializable {
     
-    protected Long txnId;
+    protected long txnId;
     protected long timestamp;
-    protected String procName;
+    protected int procId;
     protected ParameterSet procParams;
     
     /** Set to true if we know that this entry has been written to disk */
@@ -20,38 +20,29 @@ public class LogEntry implements FastSerializable {
 
     @Override
     public void readExternal(FastDeserializer in) throws IOException {
-        this.txnId = Long.valueOf(in.readLong());
-            // TODO: We need to figure out how we want to read these entries
-            // back in. I suppose we could just make a new LocalTransaction
-            // entry each time. What we really should do is recreate
-            // the StoredProcedureInvocation and then pass that into
-            // the HStoreSite so that we can replay the transaction
-            //File file = new File("filename");
-            // Create a read-only memory-mapped file
-            //FileChannel roChannel = new RandomAccessFile(file, "r").getChannel();
-
-            //ByteBuffer readonlybuffer = roChannel.map(FileChannel.MapMode.READ_ONLY, 0, (int) roChannel.size());
-            //long txn_id = in.readLong();
-            //long time = in.readLong();
-            //String procName = in.readString();
-            //ParameterSet ps = (ParameterSet)in.readObject();
-            /*FileWriter first = new FileWriter(new File("/ltmp/hstore/readbackin.log"), true);
-            first.write("<" + txn_id + "><" + time + "><" + procName + "><" + "params" + "><COMMIT>");
-            first.close();*/
+        int stupidInt = in.readInt(); //this is the amount of space the object takes up in the buffer, in case we care
+        
+        this.txnId = in.readLong();
+        this.timestamp = in.readLong();
+        this.procId = in.readInt();
+        this.procParams = in.readObject(ParameterSet.class);
+        
+        //throw new RuntimeException("stupidInt : " + stupidInt + "txnId : " + txnId + " timestamp : " + timestamp + " procId : " + procId + " procParams : " + procParams.toString());
+        
+        // TODO: We need to figure out how we want to read these entries
+        // back in. I suppose we could just make a new LocalTransaction
+        // entry each time. What we really should do is recreate
+        // the StoredProcedureInvocation and then pass that into
+        // the HStoreSite so that we can replay the transaction
     }
 
     @Override
     public void writeExternal(FastSerializer out) throws IOException {
-        out.writeLong(this.txnId.longValue());
+        out.writeLong(this.txnId);
         out.writeLong(EstTime.currentTimeMillis());
-        
-        // TODO: Remove this once we have the header stuff working...
-        out.writeString(this.procName);
-        /* If we have a header with a mapping to Procedure names to ProcIds, we can do this
-        int procId = ts.getProcedure().getId();
-        this.buffer.putInt(procId);
-        */
-        
+        out.writeInt(this.procId);
         out.writeObject(this.procParams);
+        
+        //throw new RuntimeException("txnId : " + txnId + " timestamp : " + EstTime.currentTimeMillis() + " procId : " + procId + " procParams : " + procParams.toString());
     }
 } // CLASS
