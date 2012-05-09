8f91a1b8982527a86dcfaab48b3f4dd66702e008
genevievepatterson <gen@cs.brown.edu>
2012-05-09 18:51:00 -0400

    Updated class and test for AdHocTerminal. Contians problem with JLine console reader.

--- src/frontend/edu/brown/hstore/AdHocTerminalListener.java
+++ src/frontend/edu/brown/hstore/AdHocTerminalListener.java
@@ -1,5 +1,41 @@
 package edu.brown.hstore;
 
-public class AdHocTerminalListener {
+import jline.*;
+
+import java.io.*;
+import java.util.*;
+import java.util.zip.*;
+
+import edu.brown.protorpc.AbstractEventHandler;
+
+/** MySQL Terminal **/
+public class AdHocTerminalListener{ //extends AbstractEventHandler??
+	
+	ConsoleReader m_reader;
+	
+	public AdHocTerminalListener() throws IOException{
+		//m_reader = new ConsoleReader();
+		
+		usage();
+	}
+	
+	public static void usage() {
+        System.out.println("Usage: ");
+    }
+	
+	public String getQuery() throws IOException{
+		String query = "";
+		byte[] byte_query = new byte[200];
+		//do {
+			System.in.read(byte_query);//query = m_reader.readLine("enter command > ");            
+        //} while(query != null && query.length() > 0);
+		query = new String(byte_query);
+		return query;
+	}
+	
+	public void printResult(String result){
+		System.out.println(result);
+	}
+	
 
 }
--- tests/frontend/edu/brown/hstore/TestAdHocTerminalListener.java
+++ tests/frontend/edu/brown/hstore/TestAdHocTerminalListener.java
@@ -2,13 +2,36 @@ package edu.brown.hstore;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestAdHocTerminalListener {
-
+	 
+	AdHocTerminalListener _term;
+	
+	@Before
+	public void setUp() throws Exception {
+		_term = new AdHocTerminalListener();
+	 
+	}
+	 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testIO(){
+		
+		try {
+			String q = _term.getQuery();
+			System.out.println("Query was: "+q);
+			assertTrue(q.length()>0);
+			_term.printResult(q);
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 }
