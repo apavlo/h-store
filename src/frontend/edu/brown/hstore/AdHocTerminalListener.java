5e6b284d4ca7cc83b93cac2445a9ee3a6d2c067d
genevievepatterson <patterson.genevieve@gmail.com>
2012-05-08 20:08:14 -0400

    Added new files for sql terminal.

---
+++ src/frontend/edu/brown/hstore/AdHocTerminalListener.java
@@ -0,0 +1,5 @@
+package edu.brown.hstore;
+
+public class AdHocTerminalListener {
+
+}
---
+++ tests/frontend/edu/brown/hstore/TestAdHocTerminalListener.java
@@ -0,0 +1,14 @@
+package edu.brown.hstore;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestAdHocTerminalListener {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
