60dbf280576a298133610eb02e76762be6787299
mimosally <hanxiang.zhao89@gmail.com>
2012-05-13 05:13:09 -0400

    add CombineOptimization

--- src/frontend/edu/brown/optimizer/optimizations/CombineOptimization.java
+++ src/frontend/edu/brown/optimizer/optimizations/CombineOptimization.java
@@ -1,15 +1,19 @@
 package edu.brown.optimizer.optimizations;
 
+import java.util.Collection;
+
 import org.apache.log4j.Logger;
 import org.voltdb.plannodes.AbstractPlanNode;
+import org.voltdb.plannodes.HashAggregatePlanNode;
+import org.voltdb.plannodes.ProjectionPlanNode;
 import org.voltdb.plannodes.ReceivePlanNode;
 import org.voltdb.utils.Pair;
 
 import edu.brown.logging.LoggerUtil.LoggerBoolean;
 import edu.brown.optimizer.PlanOptimizerState;
+import edu.brown.plannodes.PlanNodeUtil;
 
 /**
- * 
  * @author mimosally
  * @author pavlo
  */
@@ -23,12 +27,13 @@ public class CombineOptimization extends AbstractOptimization {
 
     @Override
     public Pair<Boolean, AbstractPlanNode> optimize(final AbstractPlanNode root) {
-        
+
         if (root instanceof ReceivePlanNode) {
             // Mark as fast combine
             // System.err.println(PlanNodeUtil.debug(root));
+            ((ReceivePlanNode) root).setFastcombine(true);
         }
-        
+
         return (Pair.of(true, root));
     }
 
