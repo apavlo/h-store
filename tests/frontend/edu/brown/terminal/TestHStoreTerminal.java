871f191475ac8f29fcd72924aaa5f79b548f7f8f
Andy Pavlo <pavlo@cs.brown.edu>
2012-05-24 23:38:40 -0400

    Working on extracting parameters for EXEC calls in HStoreTerminal

---
+++ tests/frontend/edu/brown/terminal/TestHStoreTerminal.java
@@ -0,0 +1,36 @@
+package edu.brown.terminal;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.voltdb.types.TimestampType;
+
+import junit.framework.TestCase;
+
+public class TestHStoreTerminal extends TestCase {
+
+    
+    @Test
+    public void testExtractParams() throws Exception {
+        Object params[] = {
+            12345,
+            123.45,
+            new TimestampType(),
+            "This is a string",
+        };
+        
+        StringBuilder paramStr = new StringBuilder();
+        for (Object obj : params) {
+            if (obj instanceof String || obj instanceof TimestampType) paramStr.append('"');
+            paramStr.append(obj);
+            if (obj instanceof String || obj instanceof TimestampType) paramStr.append('"');
+            paramStr.append(" ");
+        } // FOR
+        System.err.println("ORIG: " + paramStr);
+        
+        List<String> extracted = HStoreTerminal.extractParams(paramStr.toString());
+        System.err.println("EXTRACTED: " + extracted);
+        assertEquals(params.length, extracted.size());
+    }
+    
+}
