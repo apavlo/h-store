0fe8c78b17f746000cc0bc4d2fd705c6ddeca84c
Andy Pavlo <pavlo@cs.brown.edu>
2012-06-07 10:50:38 -0400

    Only create the defered work queue if we actually will need it

--- src/frontend/edu/brown/hstore/HStoreThreadManager.java
+++ src/frontend/edu/brown/hstore/HStoreThreadManager.java
@@ -36,8 +36,6 @@ public class HStoreThreadManager {
     
     private final Map<String, boolean[]> utility_affinities = new HashMap<String, boolean[]>();
     private final String utility_suffixes[] = {
-        HStoreConstants.THREAD_NAME_POSTPROCESSOR,
-        HStoreConstants.THREAD_NAME_POSTPROCESSOR,
         HStoreConstants.THREAD_NAME_DISPATCHER,
         HStoreConstants.THREAD_NAME_LISTEN,
     };
