45c46f5f09a3641a1999405e4a4ba760300d7186
Andy Pavlo <pavlo@cs.brown.edu>
2012-06-18 12:43:54 -0400

    Finally resolved issues with overriding HStoreConf parameters from either the command-line or the input file

--- tests/frontend/edu/brown/benchmark/BenchmarkController.java
+++ tests/frontend/edu/brown/benchmark/BenchmarkController.java
@@ -1660,10 +1660,8 @@ public class BenchmarkController {
         for (String host : hstore_conf.client.hosts.split("[,;]")) {
             clientHosts.add(host);
         } // FOR
-        System.err.println("1 CLIENTHOSTS: " + clientHosts);
         if (clientHosts.isEmpty())
             clientHosts.add(hstore_conf.global.defaulthost);
-        System.err.println("2 CLIENTHOSTS: " + clientHosts);
         
         // If no hosts were given, then use the defaults 
         if (siteHosts.size() == 0)
