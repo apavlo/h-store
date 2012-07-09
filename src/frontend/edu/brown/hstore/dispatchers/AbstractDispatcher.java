68346c67791fe0e476b2ff9a57f615fa339883ae
Andy Pavlo <pavlo@cs.brown.edu>
2012-07-09 10:52:52 -0400

    The AbstractDispatcher now utilizes AbstractProcessingThread

--- src/frontend/edu/brown/hstore/dispatchers/AbstractDispatcher.java
+++ src/frontend/edu/brown/hstore/dispatchers/AbstractDispatcher.java
@@ -6,9 +6,8 @@ import java.util.concurrent.LinkedBlockingDeque;
 import org.apache.log4j.Logger;
 
 import edu.brown.hstore.HStoreCoordinator;
-import edu.brown.hstore.conf.HStoreConf;
+import edu.brown.hstore.util.AbstractProcessingThread;
 import edu.brown.utils.ClassUtil;
-import edu.brown.utils.ProfileMeasurement;
 
 /**
  * A dispatcher is a asynchronous processor for a specific type of message from
@@ -16,55 +15,38 @@ import edu.brown.utils.ProfileMeasurement;
  * @author pavlo
  * @param <E>
  */
-public abstract class AbstractDispatcher<E> implements Runnable {
+public abstract class AbstractDispatcher<E> extends AbstractProcessingThread<E> {
     private static final Logger LOG = Logger.getLogger(AbstractDispatcher.class);
     
     protected final HStoreCoordinator hstore_coordinator;
-    private final ProfileMeasurement idleTime = new ProfileMeasurement("IDLE");
-    private final LinkedBlockingDeque<E> queue = new LinkedBlockingDeque<E>();
 
     
     /**
-     * @param hStoreCoordinator
+     * @param hstore_coordinator
      */
-    public AbstractDispatcher(HStoreCoordinator hStoreCoordinator) {
-        this.hstore_coordinator = hStoreCoordinator;
+    public AbstractDispatcher(HStoreCoordinator hstore_coordinator) {
+        super(hstore_coordinator.getHStoreSite(),
+              "dispatcher",
+              new LinkedBlockingDeque<E>(),
+              hstore_coordinator.getHStoreConf().site.exec_profiling);
+        this.hstore_coordinator = hstore_coordinator;
     }
     
-    @Override
-    public final void run() {
-        HStoreConf hstore_conf = this.hstore_coordinator.getHStoreConf(); 
-        this.hstore_coordinator.getHStoreSite().getThreadManager().registerProcessingThread();
-        
-        E e = null;
-        boolean profiling = hstore_conf.site.exec_profiling;
-        while (this.hstore_coordinator.isShutdownOrPrepareShutDown() == false) {
-            try {
-                if (profiling) idleTime.start();
-                e = this.queue.take();
-                if (profiling) idleTime.stop();
-            } catch (InterruptedException ex) {
-                break;
+    public final void processingCallback(E e) {
+        try {
+            this.runImpl(e);
+        } catch (Throwable ex) {
+            String dump = null;
+            if (ClassUtil.isArray(e)) {
+                dump = Arrays.toString((Object[])e);
+            } else if (e != null) {
+                dump = e.toString();
             }
-            try {
-                this.runImpl(e);
-            } catch (Throwable ex) {
-                String dump = null;
-                if (ClassUtil.isArray(e)) {
-                    dump = Arrays.toString((Object[])e);
-                } else if (e != null) {
-                    dump = e.toString();
-                }
-                LOG.warn("Failed to process queued element: " + dump, ex);
-                continue;
-            }
-        } // WHILE
+            LOG.warn("Failed to process queued element: " + dump, ex);
+        }
     }
-    public void queue(E e) {
+    public final void queue(E e) {
         this.queue.offer(e);
-    }
-    public ProfileMeasurement getIdleTime() {
-        return (this.idleTime);
     }
     
     public abstract void runImpl(E e);
