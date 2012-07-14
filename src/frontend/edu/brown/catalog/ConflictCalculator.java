55ca2adb52ce8d6f8ffad7b5b7f09896166600ff
Andy Pavlo <pavlo@cs.brown.edu>
2012-07-13 20:45:39 -0400

    Building out more of the ConflictCalculator. Going downstairs to hang out with KB and keep on working on this

--- src/frontend/edu/brown/catalog/ConflictCalculator.java
+++ src/frontend/edu/brown/catalog/ConflictCalculator.java
@@ -1,6 +1,7 @@
 package edu.brown.catalog;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -8,6 +9,8 @@ import org.apache.log4j.Logger;
 import org.voltdb.catalog.Catalog;
 import org.voltdb.catalog.Database;
 import org.voltdb.catalog.Procedure;
+import org.voltdb.catalog.Statement;
+import org.voltdb.types.QueryType;
 
 import edu.brown.logging.LoggerUtil;
 import edu.brown.logging.LoggerUtil.LoggerBoolean;
@@ -21,27 +24,40 @@ public class ConflictCalculator {
     }
     
     private class ProcedureInfo {
-        
+        final Set<Statement> readQueries = new HashSet<Statement>();
+        final Set<Statement> writeQueries = new HashSet<Statement>();
+        final Set<Procedure> conflicts = new HashSet<Procedure>();
     }
     
     private final Database catalog_db;
-    private final Map<Procedure, Set<Procedure>> procConflicts = new HashMap<Procedure, Set<Procedure>>(); 
+    private final Map<Procedure, ProcedureInfo> procedures = new HashMap<Procedure, ConflictCalculator.ProcedureInfo>(); 
     
     public ConflictCalculator(Catalog catalog) {
         this.catalog_db = CatalogUtil.getDatabase(catalog);
+
+        for (Procedure catalog_proc : catalog_db.getProcedures()) {
+            if (catalog_proc.getMapreduce() || catalog_proc.getSystemproc()) continue;
+            
+            ProcedureInfo pInfo = new ProcedureInfo();
+            for (Statement catalog_stmt : catalog_proc.getStatements()) {
+                if (catalog_stmt.getQuerytype() == QueryType.SELECT.getValue()) {
+                    pInfo.readQueries.add(catalog_stmt);
+                } else {
+                    pInfo.writeQueries.add(catalog_stmt);
+                }
+            } // FOR (statement)
+            this.procedures.put(catalog_proc, pInfo);
+        } // FOR (procedure)
     }
     
     public void process() throws Exception {
-        
         // For each Procedure, get the list of read and write queries
-        // Then check whether there is a R-W or W-R conflict with other procedures 
-        for (Procedure catalog_proc : catalog_db.getProcedures()) {
-            
-            
+        // Then check whether there is a R-W or W-R conflict with other procedures
+        for (Procedure catalog_proc : this.procedures.keySet()) {
+            ProcedureInfo pInfo = this.procedures.get(catalog_proc);
+            assert(pInfo != null);
         } // FOR
-        
-        
-        
+
     }
     
     public void updateCatalog() {
