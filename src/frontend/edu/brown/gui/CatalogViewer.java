443befa65c5d2a3bd966e70a7fe316c2345d3d94
Andy Pavlo <pavlo@cs.brown.edu>
2012-07-14 12:20:57 -0400

    Fixed formatter for Procedure conflicts in CatalogViewer

--- src/frontend/edu/brown/gui/CatalogViewer.java
+++ src/frontend/edu/brown/gui/CatalogViewer.java
@@ -34,11 +34,15 @@ import java.awt.event.FocusListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 
 import javax.swing.BoxLayout;
 import javax.swing.JLabel;
@@ -551,12 +555,13 @@ public class CatalogViewer extends AbstractViewer {
      * @param catalog_obj
      */
     protected String getAttributesText(CatalogType catalog_obj) {
-        final Map<String, Object> map = new LinkedHashMap<String, Object>();
+        Map<String, Object> m[] = (Map<String, Object>[])new Map[1];
+        m[0] = new LinkedHashMap<String, Object>();
         
 //      StringBuilder buffer = new StringBuilder();
         // buffer.append("guid: ").append(catalog_obj.getGuid()).append("\n");
-        map.put("relativeIndex", catalog_obj.getRelativeIndex());
-        map.put("nodeVersion", catalog_obj.getNodeVersion());
+        m[0].put("relativeIndex", catalog_obj.getRelativeIndex());
+        m[0].put("nodeVersion", catalog_obj.getNodeVersion());
         
         // Default Output
         if ((catalog_obj instanceof Database) == false) {
@@ -589,7 +594,7 @@ public class CatalogViewer extends AbstractViewer {
                 
                 // Default
                 Object value = catalog_obj.getField(field);
-                map.put(field, value);
+                m[0].put(field, value);
                 
                 // Specialized Output
                 if (value != null && catalog_fields.contains(field)) {
@@ -604,22 +609,22 @@ public class CatalogViewer extends AbstractViewer {
                     
                     if (catalog_item != null) {
                         boolean include_class = show_type.contains(catalog_item.getClass()); 
-                        map.put(field, CatalogUtil.getDisplayName(catalog_item, include_class));
+                        m[0].put(field, CatalogUtil.getDisplayName(catalog_item, include_class));
                     } else {
-                        map.put(field, catalog_item);
+                        m[0].put(field, catalog_item);
                     }
                 }
                 
                 // CONSTRAINT
                 else if (catalog_obj instanceof Constraint) {
                     if (field == "type") {
-                        map.put(field, ConstraintType.get((Integer)value));
+                        m[0].put(field, ConstraintType.get((Integer)value));
                     }
                 }
                 // INDEX
                 else if (catalog_obj instanceof Index) {
                     if (field == "type") {
-                        map.put(field, IndexType.get((Integer)value));
+                        m[0].put(field, IndexType.get((Integer)value));
                     }
                 }
                 // COLUMN / STMTPARAMETER / PROCPARAMETER
@@ -627,27 +632,27 @@ public class CatalogViewer extends AbstractViewer {
                     String keys[] = { "type", "sqltype", "javatype", "defaultvaluetype" };
                     for (String key : keys) {
                         if (field == key) {
-                            map.put(field, VoltType.get(((Integer)value).byteValue()).name());
+                            m[0].put(field, VoltType.get(((Integer)value).byteValue()).name());
                             break;
                         }
                     } // FOR
                     if (field.equals("procparameter")) {
                         ProcParameter proc_param = ((StmtParameter)catalog_obj).getProcparameter();
                         if (proc_param != null) {
-                            map.put(field, proc_param.fullName()); 
+                            m[0].put(field, proc_param.fullName()); 
                         }
                     }
                 }
             } // FOR
         } else {
-            map.put("project", ((Database)catalog_obj).getProject());
+            m[0].put("project", ((Database)catalog_obj).getProject());
         }
         
         // INDEX
         if (catalog_obj instanceof Index) {
             Index catalog_idx = (Index)catalog_obj;
             Collection<Column> cols = CatalogUtil.getColumns(CatalogUtil.getSortedCatalogItems(catalog_idx.getColumns(), "index"));
-            map.put("columns", CatalogUtil.getDisplayNames(cols));
+            m[0].put("columns", CatalogUtil.getDisplayNames(cols));
         }
         // CONSTRAINT
         else if (catalog_obj instanceof Constraint) {
@@ -659,31 +664,35 @@ public class CatalogViewer extends AbstractViewer {
                 Index catalog_idx = catalog_const.getIndex();
                 cols = CatalogUtil.getColumns(catalog_idx.getColumns());
             }
-            map.put("columns", CatalogUtil.getDisplayNames(cols));
+            m[0].put("columns", CatalogUtil.getDisplayNames(cols));
         }
         // COLUMN
         else if (catalog_obj instanceof Column) {
             Column catalog_col = (Column)catalog_obj;
             Collection<Constraint> consts = CatalogUtil.getConstraints(catalog_col.getConstraints());
-            map.put("constraints", CatalogUtil.getDisplayNames(consts));
+            m[0].put("constraints", CatalogUtil.getDisplayNames(consts));
         }
         // MATERIALIZEDVIEWINFO
         else if (catalog_obj instanceof MaterializedViewInfo) {
             MaterializedViewInfo catalog_view = (MaterializedViewInfo)catalog_obj;
             Collection<ColumnRef> cols = catalog_view.getGroupbycols();
-            map.put("groupbycols", CatalogUtil.debug(CatalogUtil.getColumns(cols)));
+            m[0].put("groupbycols", CatalogUtil.debug(CatalogUtil.getColumns(cols)));
         }
         // PROCEDURE
         else if (catalog_obj instanceof Procedure) {
             Procedure catalog_proc = (Procedure)catalog_obj;
-            Collection<Procedure> conflicts = CatalogUtil.getConflictProcedures(catalog_proc);
-            map.put("conflicts",
-                    StringUtil.join(", ", CatalogUtil.getDisplayNames(conflicts)));
-            map.put("conflictsX",
-                    StringUtil.join(", ", conflicts));
+            
+            Map<String, Object> orig_m = m[0];
+            m = (Map<String, Object>[])new Map[2];
+            m[0] = orig_m;
+            
+            List<String> conflicts = new ArrayList<String>(CatalogUtil.getDisplayNames(CatalogUtil.getConflictProcedures(catalog_proc)));
+            Collections.sort(conflicts);
+            m[1] = new TreeMap<String, Object>();
+            m[1].put("conflicts", StringUtil.join("\n", conflicts));
         }
         
-        StringBuilder sb = new StringBuilder(StringUtil.formatMaps(map));
+        StringBuilder sb = new StringBuilder(StringUtil.formatMaps(m));
         
         // DATABASE
         if (catalog_obj instanceof Database) {
