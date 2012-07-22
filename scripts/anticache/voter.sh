e65c1242513a9c3bb413cc5220f82025bb5fff92
Andy Pavlo <pavlo@cs.brown.edu>
2012-07-22 17:11:38 -0400

    anti-caching testing script is ready to go. Going to explore different block sizes

--- scripts/anticache/voter.sh
+++ scripts/anticache/voter.sh
@@ -11,15 +11,19 @@ function onexit() {
 
 # ---------------------------------------------------------------------
 
+ENABLE_ANTICACHE=false
+
 SITE_HOST="modis"
 
 CLIENT_HOSTS=( \
+    "modis" \
+    "modis" \
     "modis" \
     # "saw" \
     # "saw" \
 )
 
-BASE_CLIENT_THREADS=3
+BASE_CLIENT_THREADS=10
 BASE_SITE_MEMORY=2048
 BASE_SITE_MEMORY_PER_PARTITION=1024
 BASE_PROJECT="voter"
@@ -34,7 +38,7 @@ BASE_ARGS=( \
 #     "-Dsite.status_show_executor_info=true" \
 #     "-Dsite.exec_profiling=true" \
 #     "-Dsite.status_show_txn_info=true" \
-#     "-Dsite.network_profiling=true" \
+#     "-Dsite.network_profiling=false" \
 #     "-Dsite.log_backup=true"\
     
     # Site Params
@@ -43,32 +47,32 @@ BASE_ARGS=( \
     "-Dsite.exec_preprocessing_threads_count=2" \
     "-Dsite.exec_postprocessing_threads=false" \
     "-Dsite.queue_incoming_max_per_partition=500" \
-    "-Dsite.queue_incoming_increase_max=1000" \
+    "-Dsite.queue_incoming_increase_max=2000" \
     
     # Client Params
     "-Dclient.scalefactor=1" \
     "-Dclient.memory=4096" \
     "-Dclient.txnrate=2000" \
-    "-Dclient.warmup=20000" \
-    "-Dclient.duration=120000 "\
+    "-Dclient.warmup=30000" \
+    "-Dclient.duration=300000 "\
     "-Dclient.shared_connection=false" \
     "-Dclient.blocking=false" \
     "-Dclient.blocking_concurrent=100" \
     "-Dclient.throttle_backoff=100" \
-    "-Dclient.profiling=true" \
-
+    
     # Anti-Caching Experiments
-    "-Dsite.anticache_enable=true" \
+    "-Dsite.anticache_enable=${ENABLE_ANTICACHE}" \
     "-Dsite.anticache_check_interval=99999999" \
     "-Dclient.interval=500" \
-    "-Dclient.output_csv=true" \
-    "-Dclient.anticache_enable=true" \
+    "-Dclient.anticache_enable=${ENABLE_ANTICACHE}" \
     "-Dclient.anticache_evict_interval=30000" \
     "-Dclient.anticache_evict_size=4194304" \
     "-Dclient.output_csv=true" \
+    "-Dclient.output_interval=false" \
 
     # CLIENT DEBUG
-#     "-Dclient.output_response_status=true" \
+    "-Dclient.profiling=false" \
+    "-Dclient.output_response_status=true" \
 #     "-Dclient.output_basepartitions=true" \
 #     "-Dclient.jvm_args=\"-verbose:gc -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:-TraceClassUnloading\"" \
 )
@@ -83,15 +87,14 @@ done
 
 # ant compile
 # for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16; do
-for i in 8; do
+for i in 4; do
 
     HSTORE_HOSTS="${SITE_HOST}:0:0-"`expr $i - 1`
-#     NUM_CLIENTS=`expr $i \* $BASE_CLIENT_THREADS`
-#     NUM_CLIENTS=$BASE_CLIENT_THREADS
+    NUM_CLIENTS=`expr $i \* $BASE_CLIENT_THREADS`
 #     if [ $i -gt 1 ]; then
-    NUM_CLIENTS=`expr \( $i - 1 \) \* $BASE_CLIENT_THREADS`
+#         NUM_CLIENTS=`expr \( $i - 1 \) \* $BASE_CLIENT_THREADS`
 #     else
-#         NUM_CLIENTS=`expr \( $i + 1 \) \* $BASE_CLIENT_THREADS`
+#         NUM_CLIENTS=$BASE_CLIENT_THREADS
 #     fi
     SITE_MEMORY=`expr $BASE_SITE_MEMORY + \( $i \* $BASE_SITE_MEMORY_PER_PARTITION \)`
     
