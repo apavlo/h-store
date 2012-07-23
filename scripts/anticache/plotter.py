a1d11db9f05119454301c63de5db478e113d732a
Andy Pavlo <pavlo@cs.brown.edu>
2012-07-23 10:25:20 -0400

    Commiting latest version of voter anti-cache graph script

--- scripts/anticache/plotter.py
+++ scripts/anticache/plotter.py
@@ -15,23 +15,30 @@ OPT_GRAPH_DPI = 100
 ## main
 ## ==============================================
 if __name__ == '__main__':
-
-    plot.xlabel('time')
-    plot.ylabel('transactions per second')
-    #ax = plot.gca()
-    #ax.set_autoscaley_on(False)
     
     input_files = [
-        #("VOTER-4p-noevict.csv",   "No Eviction",    "red"),
-        #("VOTER-4p-evict-4MB.csv", "Eviction (4MB)", "blue"),
-        ("VOTER-4p-evict-8MB.csv", "Eviction (8MB)", "orange"),
+        (4, "#D20040"),
+        (8, "#01299F"),
+        (16, "#EBBF00"),
+        (32, "#5A001B"),
+        (64, "#655200"),
     ]
-    evictions = set()
-    y_max = None
+    
 
-    first = True
-    for file, label, color in input_files:
-        with open(file, "r") as f:
+    for size, color in input_files:
+        evict_size = "%dMB" % size
+        label = "VOTER (Eviction Block Size %s)" % evict_size
+        
+        plot.clf()
+        plot.xlabel('time')
+        plot.ylabel('transactions per second')
+        #ax = plot.gca()
+        #ax.set_autoscaley_on(False)
+        evictions = { }
+        y_max = None
+
+        first = True
+        with open("VOTER-4p-evict-%s.csv" % evict_size, "r") as f:
             reader = csv.reader(f)
             
             col_xref = None
@@ -50,40 +57,40 @@ if __name__ == '__main__':
                 y_values.append(float(row[col_xref['THROUGHPUT']]))
                 
                 y_max = max(y_max, y_values[-1])
-                if first and row[col_xref["EVICTING"]] == "true":
-                    evictions.add(x_values[-1])
+                if first and int(row[col_xref["EVICTING"]]) != 0:
+                    evictions[x_values[-1]] = int(row[col_xref["EVICTING"]]) / 1000.0
             ## FOR
             plot.plot(x_values, y_values, label=label, color=color)
         ## WITH
-        first = len(evictions) == 0
-    ## FOR
 
-    if len(evictions) > 0:
-        for x in evictions:
-            plot.vlines(x, 0, y_max, color='black', linewidth=2.0, linestyles='dashed')
-    ## IF
-    
-    F = pylab.gcf()
-    #F.autofmt_xdate()
+        if len(evictions) > 0:
+            y_max = max(y_max, 120000)
+            for x,width in evictions.iteritems():
+                width = max(1, width)
+                plot.vlines(x, 0, y_max, color='black', linewidth=width, linestyles='dashed', alpha=1.0)
+        ## IF
+        
+        F = pylab.gcf()
+        #F.autofmt_xdate()
 
-    # Now check everything with the defaults:
-    size = F.get_size_inches()
-    dpi = F.get_dpi()
-    logging.debug("Current Size Inches: %s, DPI: %d" % (str(size), dpi))
-    
-    new_size = (OPT_GRAPH_WIDTH / float(dpi), OPT_GRAPH_HEIGHT / float(dpi))
-    F.set_size_inches(new_size)
-    F.set_dpi(OPT_GRAPH_DPI)
-    
-    # Now check everything with the defaults:
-    size = F.get_size_inches()
-    dpi = F.get_dpi()
-    logging.debug("Current Size Inches: %s, DPI: %d" % (str(size), dpi))
-    
-    new_size = (OPT_GRAPH_WIDTH / float(dpi), OPT_GRAPH_HEIGHT / float(dpi))
-    F.set_size_inches(new_size)
-    F.set_dpi(OPT_GRAPH_DPI)
+        # Now check everything with the defaults:
+        size = F.get_size_inches()
+        dpi = F.get_dpi()
+        logging.debug("Current Size Inches: %s, DPI: %d" % (str(size), dpi))
+        
+        new_size = (OPT_GRAPH_WIDTH / float(dpi), OPT_GRAPH_HEIGHT / float(dpi))
+        F.set_size_inches(new_size)
+        F.set_dpi(OPT_GRAPH_DPI)
+        
+        # Now check everything with the defaults:
+        size = F.get_size_inches()
+        dpi = F.get_dpi()
+        logging.debug("Current Size Inches: %s, DPI: %d" % (str(size), dpi))
+        
+        new_size = (OPT_GRAPH_WIDTH / float(dpi), OPT_GRAPH_HEIGHT / float(dpi))
+        F.set_size_inches(new_size)
+        F.set_dpi(OPT_GRAPH_DPI)
 
-    plot.legend(loc='upper right')
-    plot.savefig("voter-eviction.png")
+        plot.legend(loc='upper right')
+        plot.savefig("voter-eviction-%s.png" % evict_size)
 ## MAIN
