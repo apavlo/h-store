48b50cfca51753d9278ed17be41562e253136cc8
Andy Pavlo <pavlo@cs.brown.edu>
2012-07-25 10:27:51 -0400

    More tweaking of the ConflictGraph in the CatalogViewer. Ok I'm done for real for now...

--- src/frontend/edu/brown/gui/catalog/ProcedureConflictGraphNode.java
+++ src/frontend/edu/brown/gui/catalog/ProcedureConflictGraphNode.java
@@ -25,13 +25,15 @@ import edu.brown.graphs.AbstractVertex;
 import edu.brown.gui.common.GraphVisualizationPanel;
 import edu.brown.utils.CollectionUtil;
 import edu.uci.ics.jung.graph.util.EdgeType;
+import edu.uci.ics.jung.graph.util.Pair;
+import edu.uci.ics.jung.visualization.RenderContext;
 import edu.uci.ics.jung.visualization.picking.PickedState;
 import edu.uci.ics.jung.visualization.renderers.Renderer.VertexLabel.Position;
 
 public class ProcedureConflictGraphNode {
     private static final Logger LOG = Logger.getLogger(ProcedureConflictGraphNode.class);
     
-    static final Map<ConflictType, Paint> CONFLICT_COLORS = new HashMap<ConflictType, Paint>();
+    static final Map<ConflictType, Color> CONFLICT_COLORS = new HashMap<ConflictType, Color>();
     static {
         CONFLICT_COLORS.put(ConflictType.READ_WRITE, Color.GREEN.darker());
         CONFLICT_COLORS.put(ConflictType.WRITE_WRITE, Color.BLUE.darker());
@@ -43,10 +45,20 @@ public class ProcedureConflictGraphNode {
     final ConflictGraph graph;
     GraphVisualizationPanel<ConflictVertex, ConflictEdge> vizPanel;
     
+    // -----------------------------------------------------------------
+    // TRANSFORMERS
+    // -----------------------------------------------------------------
+    
     final Transformer<ConflictEdge, Paint> edgeColor = new Transformer<ConflictEdge, Paint>() {
         @Override
         public Paint transform(ConflictEdge e) {
-            return CONFLICT_COLORS.get(e.type);
+            Color c = CONFLICT_COLORS.get(e.type);
+            PickedState<ConflictVertex> pickedState = vizPanel.getPickedVertexState();
+            Pair<ConflictVertex> p = graph.getEndpoints(e);
+            if (pickedState.isPicked(p.getFirst())) {
+                c = c.darker().darker();
+            }
+            return c;
         }
     };
     final Transformer<ConflictEdge, Stroke> edgeStroke = new Transformer<ConflictEdge, Stroke>() {
@@ -57,14 +69,46 @@ public class ProcedureConflictGraphNode {
         }
     };
     final Transformer<ConflictVertex, Paint> vertexColor = new Transformer<ConflictVertex, Paint>() {
-        final Paint picked = new Color(225, 225, 225);
-        final Paint unpicked = new Color(175, 175, 175);
+        final Paint picked = new Color(175, 175, 175);
+        final Paint unpicked = new Color(225, 225, 225);
         @Override
         public Paint transform(ConflictVertex v) {
             PickedState<ConflictVertex> pickedState = vizPanel.getPickedVertexState();
             return (pickedState.isPicked(v) ? picked : unpicked);
         }
     };
+    final Transformer<ConflictVertex, Paint> vertexLine = new Transformer<ConflictVertex, Paint>() {
+        final Paint picked = Color.RED.darker();
+        final Paint unpicked = Color.BLACK;
+        @Override
+        public Paint transform(ConflictVertex v) {
+            PickedState<ConflictVertex> pickedState = vizPanel.getPickedVertexState();
+            return (pickedState.isPicked(v) ? picked : unpicked);
+        }
+    };
+    final Transformer<ConflictVertex, Shape> vertexShape = new Transformer<ConflictVertex, Shape>() {
+        int width = 160;  int height = 40;
+        int x = width/-2; int y = height/-2;
+        @Override
+        public Shape transform(ConflictVertex v) {
+            return new Rectangle(x, y, width, height);
+        }
+    };
+    final Transformer<ConflictVertex, String> vertexLabel = new Transformer<ConflictVertex, String>() {
+        final String picked = "#C80000";
+        final String unpicked = "#000000";
+        @Override
+        public String transform(ConflictVertex v) {
+            PickedState<ConflictVertex> pickedState = vizPanel.getPickedVertexState();
+            String c = (pickedState.isPicked(v) ? picked : unpicked);
+            return String.format("<html><font color=\"%s\">%s</font></html>", c, v.toString());
+        }
+    };
+
+    
+    // -----------------------------------------------------------------
+    // INITIALIZATION
+    // -----------------------------------------------------------------
     
     public ProcedureConflictGraphNode(Collection<Procedure> procs) {
         this.catalog_db = CatalogUtil.getDatabase(CollectionUtil.first(procs));
@@ -101,26 +145,17 @@ public class ProcedureConflictGraphNode {
         }
         
         this.vizPanel = GraphVisualizationPanel.factory(this.graph);
-        this.vizPanel.getRenderContext().setEdgeDrawPaintTransformer(this.edgeColor);
-        this.vizPanel.getRenderContext().setArrowDrawPaintTransformer(this.edgeColor);
-        this.vizPanel.getRenderContext().setArrowFillPaintTransformer(this.edgeColor);
-        this.vizPanel.getRenderContext().setVertexFillPaintTransformer(this.vertexColor);
-        this.vizPanel.getRenderContext().setEdgeStrokeTransformer(this.edgeStroke);
-        this.vizPanel.getRenderContext().setVertexFontTransformer(new GraphVisualizationPanel.VertexFontTransformer<ProcedureConflictGraphNode.ConflictVertex>(true));
+        RenderContext<ConflictVertex, ConflictEdge> rc = this.vizPanel.getRenderContext(); 
+        rc.setEdgeDrawPaintTransformer(this.edgeColor);
+        rc.setEdgeStrokeTransformer(this.edgeStroke);
+        rc.setArrowDrawPaintTransformer(this.edgeColor);
+        rc.setArrowFillPaintTransformer(this.edgeColor);
+        rc.setVertexFillPaintTransformer(this.vertexColor);
+        rc.setVertexDrawPaintTransformer(this.vertexLine);
+        rc.setVertexShapeTransformer(this.vertexShape);
+        rc.setVertexLabelTransformer(this.vertexLabel);
+        rc.setVertexFontTransformer(new GraphVisualizationPanel.VertexFontTransformer<ConflictVertex>(true));
         this.vizPanel.getRenderer().getVertexLabelRenderer().setPosition(Position.CNTR);
-        
-        this.vizPanel.getRenderContext().setVertexShapeTransformer(
-            new Transformer<ConflictVertex, Shape>() {
-                int width = 160;  int height = 40;
-                int x = width/-2; int y = height/-2;
-                @Override
-                public Shape transform(ConflictVertex v) {
-                    return new Rectangle(x, y, width, height);
-                }
-            }
-        );
-        
-        
     }
     
     public JPanel getVisualization() {
