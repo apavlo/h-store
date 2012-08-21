a2f3f5b0a315b9eb6a8f594162b4f35d295c4825
Andy Pavlo <pavlo@cs.brown.edu>
2012-08-21 13:55:52 -0400

    Forgot to initialize m_nonInlinedMemorySize in the EE's tables

--- src/ee/storage/table.cpp
+++ src/ee/storage/table.cpp
@@ -69,6 +69,8 @@ Table::Table(int tableAllocationTargetSize) :
     m_allocatedTuples(0),
     m_columnCount(0),
     m_tuplesPerBlock(0),
+    m_tupleLength(0),
+    m_nonInlinedMemorySize(0),
     m_columnHeaderData(NULL),
     m_columnHeaderSize(-1),
     m_columnNames(NULL),
