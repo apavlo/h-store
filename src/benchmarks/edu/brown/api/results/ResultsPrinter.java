8b7e1308aadb6141d7f3fe8826c31fcda00edfe2
Andy Pavlo <pavlo@cs.brown.edu>
2013-01-04 12:35:25 -0500

    Always print the % of speculative txns in the final benchmark results

--- src/benchmarks/edu/brown/api/results/ResultsPrinter.java
+++ src/benchmarks/edu/brown/api/results/ResultsPrinter.java
@@ -114,15 +114,12 @@ public class ResultsPrinter implements BenchmarkInterest {
              .append(String.format("stdev:" + RESULT_FORMAT, fr.getTotalStdDevLatency()))
              .append("]");
         
-        String txnInfo = String.format("Total:%d / Distributed:%d (%.1f%%)",
+        String txnInfo = String.format("Total:%d / Distributed:%d (%.1f%%) / SpecExec:%d (%.1f%%)",
                                         fr.getTotalTxnCount(),
                                         fr.getTotalDtxnCount(),
-                                        (fr.getTotalDtxnCount() / (double)fr.getTotalTxnCount())*100);
-        if (fr.getTotalSpecExecCount() > 0) {
-            txnInfo += String.format(" / SpecExec:%d (%.1f%%)",
+                                        (fr.getTotalDtxnCount() / (double)fr.getTotalTxnCount())*100,
                                         fr.getTotalSpecExecCount(),
                                         (fr.getTotalSpecExecCount() / (double)fr.getTotalTxnCount())*100);
-        }
         
         
         Map<String, Object> m = new LinkedHashMap<String, Object>();
