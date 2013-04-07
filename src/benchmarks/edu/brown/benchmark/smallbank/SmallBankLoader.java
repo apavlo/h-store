a4a106682217d105f66ddf3c0fcf7ead316d2904
Andy Pavlo <pavlo@cs.brown.edu>
2013-04-07 15:24:10 -0400

    More work porting the SmallBank benchmark to H-Store. We now have the loader and the first transaction type working...

--- src/benchmarks/edu/brown/benchmark/smallbank/SmallBankLoader.java
+++ src/benchmarks/edu/brown/benchmark/smallbank/SmallBankLoader.java
@@ -30,9 +30,23 @@
 
 package edu.brown.benchmark.smallbank;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.apache.log4j.Logger;
+import org.voltdb.CatalogContext;
+import org.voltdb.VoltTable;
+import org.voltdb.VoltType;
+import org.voltdb.catalog.Column;
+import org.voltdb.catalog.Table;
+import org.voltdb.utils.CatalogUtil;
 
 import edu.brown.api.Loader;
+import edu.brown.rand.DefaultRandomGenerator;
+import edu.brown.rand.RandomDistribution.DiscreteRNG;
+import edu.brown.rand.RandomDistribution.Gaussian;
+import edu.brown.utils.ThreadUtil;
 
 public class SmallBankLoader extends Loader {
     private static final Logger LOG = Logger.getLogger(SmallBankLoader.class);
@@ -41,13 +55,106 @@ public class SmallBankLoader extends Loader {
     public static void main(String args[]) throws Exception {
         Loader.main(SmallBankLoader.class, args, true);
     }
-
+    
+    private final long numAccounts;
+    private int loadthreads = ThreadUtil.availableProcessors();
+    private final int acctNameLength;
+    
     public SmallBankLoader(String[] args) {
         super(args);
+        
+        this.numAccounts = (int)Math.round(SmallBankConstants.NUM_ACCOUNTS * 
+                                           this.getScaleFactor());
+        
+        // Calculate account name length
+        CatalogContext catalogContext = this.getCatalogContext();
+        Table catalog_tbl = catalogContext.getTableByName(SmallBankConstants.TABLENAME_ACCOUNTS);
+        int acctNameLength = -1;
+        for (Column col : catalog_tbl.getColumns()) {
+            if (col.getType() == VoltType.STRING.getValue()) {
+                acctNameLength = col.getSize();
+                break;
+            }
+        } // FOR
+        assert(acctNameLength > 0);
+        this.acctNameLength = acctNameLength;
     }
 
     @Override
     public void load() {
-
+        final CatalogContext catalogContext = this.getCatalogContext();
+        final int rows_per_thread = (int)Math.ceil(this.numAccounts / (double)this.loadthreads);
+        final List<Runnable> runnables = new ArrayList<Runnable>();
+        for (int i = 0; i < this.loadthreads; i++) {
+            final int start = rows_per_thread * i;
+            final int stop = start + rows_per_thread;
+            runnables.add(new Generator(catalogContext, start, stop));
+        } // FOR
+        ThreadUtil.runGlobalPool(runnables);
+        
+        System.err.println(this.getTableTupleCounts());
     }
+    
+    /**
+     * Thread that can generate a range of accounts
+     */
+    private class Generator implements Runnable {
+        private final VoltTable acctsTable;
+        private final VoltTable savingsTable;
+        private final VoltTable checkingTable;
+        private final int start;
+        private final int stop;
+        private final DefaultRandomGenerator rand = new DefaultRandomGenerator(); 
+        private final DiscreteRNG randBalance;
+        
+        public Generator(CatalogContext catalogContext, int start, int stop) {
+            this.acctsTable = CatalogUtil.getVoltTable(catalogContext.getTableByName(SmallBankConstants.TABLENAME_ACCOUNTS));
+            this.savingsTable = CatalogUtil.getVoltTable(catalogContext.getTableByName(SmallBankConstants.TABLENAME_SAVINGS));
+            this.checkingTable = CatalogUtil.getVoltTable(catalogContext.getTableByName(SmallBankConstants.TABLENAME_CHECKING));
+            this.start = start;
+            this.stop = stop;
+            this.randBalance = new Gaussian(this.rand,
+                                            SmallBankConstants.MIN_BALANCE,
+                                            SmallBankConstants.MAX_BALANCE);
+        }
+        
+        public void run() {
+            final String acctNameFormat = "%0"+acctNameLength+"d";
+            int batchSize = 0;
+            for (int acctId = this.start; acctId < this.stop; acctId++) {
+                // ACCOUNT
+                String acctName = String.format(acctNameFormat, acctId);
+                this.acctsTable.addRow(acctId, acctName);
+                
+                // CHECKINGS
+                if (rand.nextInt(100) > SmallBankConstants.PERCENTAGE_NO_CHECKING) {
+                    double balance = this.randBalance.nextDouble();
+                    this.checkingTable.addRow(acctId, balance);
+                }
+                
+                // SAVINGS
+                if (rand.nextInt(100) > SmallBankConstants.PERCENTAGE_NO_SAVINGS) {
+                    double balance = this.randBalance.nextDouble();
+                    this.savingsTable.addRow(acctId, balance);
+                }
+                
+                if (++batchSize >= SmallBankConstants.BATCH_SIZE) {
+                    this.loadTables();
+                    batchSize = 0;
+                }
+            } // FOR
+            if (batchSize > 0) {
+                this.loadTables();
+            }
+        }
+        
+        private void loadTables() {
+            loadVoltTable(SmallBankConstants.TABLENAME_ACCOUNTS, this.acctsTable);
+            this.acctsTable.clearRowData();
+            loadVoltTable(SmallBankConstants.TABLENAME_SAVINGS, this.savingsTable);
+            this.savingsTable.clearRowData();
+            loadVoltTable(SmallBankConstants.TABLENAME_CHECKING, this.checkingTable);
+            this.checkingTable.clearRowData();
+        }
+    };
 }
