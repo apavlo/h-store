7b45eeef7420f1b2dae503964b049232913821e6
Aaron E <mettamara@gmail.com>
2013-04-11 20:28:35 -0700

    Placeholder for partition plan hasher

---
+++ src/frontend/edu/brown/hashing/PlannedHasher.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package edu.brown.hashing;
+
+import org.voltdb.catalog.CatalogType;
+import org.voltdb.catalog.Database;
+import org.voltdb.catalog.Procedure;
+import org.voltdb.catalog.Table;
+import org.voltdb.utils.NotImplementedException;
+
+/**
+ * @author aelmore Hasher that uses a planned partition plan, stored in the
+ *         database catalog. This partition plan can change over time
+ * 
+ */
+public class PlannedHasher extends DefaultHasher {
+
+  /**
+   * @param catalog_db
+   * @param num_partitions
+   */
+  public PlannedHasher(Database catalog_db, int num_partitions) {
+    super(catalog_db, num_partitions);
+  }
+
+  /**
+   * @param catalog_db
+   */
+  public PlannedHasher(Database catalog_db) {
+    super(catalog_db);
+  }
+
+  @Override
+  public int hash(Object value) {
+    throw new NotImplementedException("Hashing without Catalog not supported");
+  }
+
+  @Override
+  public int hash(Object value, CatalogType catalogItem) {
+    if (catalogItem instanceof Table) {
+      // TODO
+    } else if (catalogItem instanceof Procedure) {
+      // TODO
+    }
+    throw new NotImplementedException("TODO");
+  }
+
+  @Override
+  public int hash(Object value, int num_partitions) {
+    throw new NotImplementedException("Hashing without Catalog not supported");
+  }
+
+}
