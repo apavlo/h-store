fc07853dca200e3267a5d5f0e72f308370075993
Vaibhav Arora <vaibhavarora.nitt@gmail.com>
2013-05-08 18:59:57 -0700

    Inclusing Callback in the Pull Request Message

--- src/frontend/edu/brown/hstore/internal/LivePullRequestMessage.java
+++ src/frontend/edu/brown/hstore/internal/LivePullRequestMessage.java
@@ -1,19 +1,29 @@
 package edu.brown.hstore.internal;
 
+import com.google.protobuf.RpcCallback;
+
 import edu.brown.hstore.Hstoreservice.LivePullRequest;
+import edu.brown.hstore.Hstoreservice.LivePullResponse;
 import edu.brown.hstore.txns.AbstractTransaction;
 
 public class LivePullRequestMessage extends InternalTxnMessage {
 
   LivePullRequest livePullRequest;
+  RpcCallback<LivePullResponse> livePullResponseCallback;
   
-  public LivePullRequestMessage(AbstractTransaction ts, LivePullRequest livePullRequest) {
+  public LivePullRequestMessage(AbstractTransaction ts, LivePullRequest livePullRequest,
+      RpcCallback<LivePullResponse> livePullResponseCallback) {
     super(ts);
     this.livePullRequest = livePullRequest;
+    this.livePullResponseCallback = livePullResponseCallback;
   }
   
   public LivePullRequest getLivePullRequest() {
     return this.livePullRequest;
+  }
+  
+  public RpcCallback<LivePullResponse> getLivePullResponseCallback(){
+    return (this.livePullResponseCallback);
   }
 
 }
