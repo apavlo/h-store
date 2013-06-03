82bc4029b5110bde0e1e19fdee1741e6ddbc45bb
John Meehan <john@cs.brown.edu>
2013-06-03 17:21:27 -0400

    testing commits to branch #130

--- src/frontend/org/voltdb/compiler/VoltCompiler.java
+++ src/frontend/org/voltdb/compiler/VoltCompiler.java
@@ -584,7 +584,7 @@ public class VoltCompiler {
         m_catalog.getClusters().get("cluster").setSecurityenabled(false);
     }
 
-    //testing
+    //testing 2
     void compileDatabaseNode(DatabaseType database) throws VoltCompilerException {
         final ArrayList<String> programs = new ArrayList<String>();
         final ArrayList<String> schemas = new ArrayList<String>();
