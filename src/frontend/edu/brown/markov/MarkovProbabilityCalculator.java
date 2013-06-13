25597bead99a96dec453773b5b860e4f663539f6
Andy Pavlo <pavlo@cs.brown.edu>
2013-06-13 00:59:38 -0400

    Corrected logic in MarkovProbabilityCalculator for dealing with non-readonly successors in models

--- src/frontend/edu/brown/markov/MarkovProbabilityCalculator.java
+++ src/frontend/edu/brown/markov/MarkovProbabilityCalculator.java
@@ -1,6 +1,5 @@
 package edu.brown.markov;
 
-import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -53,20 +52,20 @@ public class MarkovProbabilityCalculator extends VertexTreeWalker<MarkovVertex, 
     @Override
     protected void callback_first(MarkovVertex element) {
         super.callback_first(element);
-        if (debug.val) {
+        if (trace.val) {
             String msg = String.format("START %s %s",
                                        this.getClass().getSimpleName(), this.getGraph());
-            LOG.debug(StringUtil.header(msg, "=", 150));
+            LOG.trace(StringUtil.header(msg, "=", 150));
         }
     }
     
     @Override
     protected void callback_finish() {
         super.callback_finish();
-        if (debug.val) {
+        if (trace.val) {
             String msg = String.format("STOP %s %s",
                                        this.getClass().getSimpleName(), this.getGraph());
-            LOG.debug(StringUtil.header(msg, "=", 150));
+            LOG.trace(StringUtil.header(msg, "=", 150));
         }
     }
     
@@ -169,39 +168,46 @@ public class MarkovProbabilityCalculator extends VertexTreeWalker<MarkovVertex, 
                         if (successorStmt.getReadonly()) {
                             if (trace.val)
                                 LOG.trace(String.format("%s does not modify partition %d. " +
-                                		  "Setting writing probability based on children [%s]",
+                                		  "Setting WRITE probability based on children [%s]",
                                           element, partition, successorType));
-                            est.addWriteProbability(partition, (e.getProbability() * successor.getWriteProbability(partition)));
-                            est.addReadOnlyProbability(partition, (e.getProbability() * successor.getReadOnlyProbability(partition)));
+                            est.addWriteProbability(partition, e.getProbability()); //  * successor.getWriteProbability(partition)));
+                            // est.addReadOnlyProbability(partition, 1.0f - e.getProbability()); //  * successor.getReadOnlyProbability(partition)));
                         } else {
                             if (trace.val)
                                 LOG.trace(String.format("%s modifies partition %d. " +
-                                		  "Setting writing probability to 1.0 [%s]",
-                                          element, partition, successorType));
-                            est.setWriteProbability(partition, 1.0f);
-                            est.setReadOnlyProbability(partition, 0.0f);
+                                		  "Setting WRITE probability to 1.0 [%s]",
+                                          successor, partition, successorType));
+                            // est.addWriteProbability(partition, 1.0f - e.getProbability());
+                            est.addReadOnlyProbability(partition, e.getProbability());
+                            // est.setWriteProbability(partition, 1.0f);
+                            // est.setReadOnlyProbability(partition, 0.0f);
                         }
                     }
                     // This successor doesn't directly access this partition, so the successor's probabilities are
                     // multiplied based on the edge probabilities 
                     else {
+                        float before;
+                        
+                        before = est.getDoneProbability(partition);
                         try {
                             est.addDoneProbability(partition, (e.getProbability() * successor.getDoneProbability(partition)));
                         } catch (Throwable ex) {
-                            LOG.warn(String.format("Failed to set FINISH probability for %s [partition=%d / edge=%s / successor=%s]",
-                                                   est, partition, e, successor), ex);
+                            LOG.warn(String.format("Failed to set FINISH probability for %s [partition=%d / edge=%s / successor=%s / before=%f]",
+                                                   est, partition, e, successor, before), ex);
                         }
+                        before = est.getWriteProbability(partition);
                         try {
                             est.addWriteProbability(partition, (e.getProbability() * successor.getWriteProbability(partition)));
                         } catch (Throwable ex) {
-                            LOG.warn(String.format("Failed to set WRITE probability for %s [partition=%d / edge=%s / successor=%s]",
-                                                   est, partition, e, successor), ex);
+                            LOG.warn(String.format("Failed to set WRITE probability for %s [partition=%d / edge=%s / successor=%s / before=%f]",
+                                                   est, partition, e, successor, before), ex);
                         }
+                        before = est.getReadOnlyProbability(partition);
                         try {
                             est.addReadOnlyProbability(partition, (e.getProbability() * successor.getReadOnlyProbability(partition)));
                         } catch (Throwable ex) {
-                            LOG.warn(String.format("Failed to set READ-ONLY probability for %s [partition=%d / edge=%s / successor=%s]",
-                                                   est, partition, e, successor), ex);
+                            LOG.warn(String.format("Failed to set READ-ONLY probability for %s [partition=%d / edge=%s / successor=%s / before=%f]",
+                                                   est, partition, e, successor, before), ex);
                         }
                     }
                 } // FOR (PartitionId)
