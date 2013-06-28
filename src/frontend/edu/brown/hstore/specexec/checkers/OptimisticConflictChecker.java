2f56aac204048c0556dcd0d3d3326b7ae3748a39
Andy Pavlo <pavlo@cs.brown.edu>
2013-06-28 16:20:52 -0400

    Added sanity checks in OptimisticConflictChecker.hasConflictAfter(). Still need to track down our out-of-order rollbacks with OCC

--- src/frontend/edu/brown/hstore/specexec/checkers/OptimisticConflictChecker.java
+++ src/frontend/edu/brown/hstore/specexec/checkers/OptimisticConflictChecker.java
@@ -1,5 +1,6 @@
 package edu.brown.hstore.specexec.checkers;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
@@ -60,6 +61,7 @@ public class OptimisticConflictChecker extends AbstractConflictChecker {
         assert(ts1.isInitialized()) :
             String.format("Uninitialized speculative transaction handle [%s]", ts1);
         
+        // Get the READ/WRITE tracking sets from the EE
         VoltTable tsTracking0[] = this.getReadWriteSets(ts0); // TODO: Cache this somehow!
         if (trace.val)
             LOG.trace(String.format("%s READ/WRITE SETS:\n%s", ts0, VoltTableUtil.format(tsTracking0)));
@@ -72,10 +74,32 @@ public class OptimisticConflictChecker extends AbstractConflictChecker {
         // say that everything kosher.
         if ((tsTracking0[READ] == null || tsTracking0[READ].getRowCount() == 0) && 
             (tsTracking0[WRITE] == null || tsTracking0[WRITE].getRowCount() == 0)) {
+            // SANITY CHECK
+            assert((tsTracking0[READ] == null || tsTracking0[READ].getRowCount() == 0) &&
+                   (ts0.getTableIdsMarkedRead(partitionId).length == 0)) :
+                String.format("%s READ Set: %s\nTables Read: %s",
+                              ts0, tsTracking0[READ],
+                              Arrays.toString(ts0.getTableIdsMarkedRead(partitionId)));
+            assert((tsTracking0[WRITE] == null || tsTracking0[WRITE].getRowCount() == 0) &&
+                   (ts0.getTableIdsMarkedWritten(partitionId).length == 0)) :
+                 String.format("%s WRITE Set: %s\nTables Written: %s",
+                               ts0, tsTracking0[WRITE],
+                               Arrays.toString(ts0.getTableIdsMarkedWritten(partitionId)));
             return (false);
         }
         else if ((tsTracking1[READ] == null || tsTracking1[READ].getRowCount() == 0) && 
-                (tsTracking1[WRITE] == null || tsTracking1[WRITE].getRowCount() == 0)) { 
+                (tsTracking1[WRITE] == null || tsTracking1[WRITE].getRowCount() == 0)) {
+            // SANITY CHECK
+            assert((tsTracking1[READ] == null || tsTracking1[READ].getRowCount() == 0) &&
+                   (ts1.getTableIdsMarkedRead(partitionId).length == 0)) :
+                 String.format("%s READ Set: %s\nTables Read: %s",
+                               ts1, tsTracking1[READ],
+                               Arrays.toString(ts1.getTableIdsMarkedRead(partitionId)));
+             assert((tsTracking1[WRITE] == null || tsTracking1[WRITE].getRowCount() == 0) &&
+                    (ts1.getTableIdsMarkedWritten(partitionId).length == 0)) :
+                  String.format("%s WRITE Set: %s\nTables Written: %s",
+                                ts1, tsTracking1[WRITE],
+                                Arrays.toString(ts1.getTableIdsMarkedWritten(partitionId)));
             return (false);
         }
 
@@ -100,6 +124,17 @@ public class OptimisticConflictChecker extends AbstractConflictChecker {
         return (false);
     }
     
+    /**
+     * Returns true if there is a conflict between the two transactions for the
+     * given list of tableIds based on their tracking sets.
+     * @param partition
+     * @param tableIds
+     * @param ts0
+     * @param tsTracking0
+     * @param ts1
+     * @param tsTracking1
+     * @return
+     */
     protected boolean hasTupleConflict(int partition, int tableIds[],
                                        AbstractTransaction ts0, VoltTable tsTracking0,
                                        AbstractTransaction ts1, VoltTable tsTracking1) {
