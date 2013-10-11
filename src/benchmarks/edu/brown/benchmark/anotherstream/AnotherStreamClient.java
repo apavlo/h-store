aa5f09a2d0a5debed205f8006aefb56f4a880a53
hawkwang <wanghao.buaa@gmail.com>
2013-10-10 21:54:57 -0400

    finish anotherstream benchmark for voter case

--- src/benchmarks/edu/brown/benchmark/anotherstream/AnotherStreamClient.java
+++ src/benchmarks/edu/brown/benchmark/anotherstream/AnotherStreamClient.java
@@ -20,6 +20,18 @@ public class AnotherStreamClient extends BenchmarkComponent {
     private static final Logger LOG = Logger.getLogger(AnotherStreamClient.class);
     private static final LoggerBoolean debug = new LoggerBoolean();
 
+    // Phone number generator
+    PhoneCallGenerator switchboard;
+
+    // Flags to tell the worker threads to stop or go
+    AtomicBoolean warmupComplete = new AtomicBoolean(false);
+    AtomicBoolean benchmarkComplete = new AtomicBoolean(false);
+
+    // voter benchmark state
+    AtomicLong acceptedVotes = new AtomicLong(0);
+    AtomicLong badContestantVotes = new AtomicLong(0);
+    AtomicLong badVoteCountVotes = new AtomicLong(0);
+    AtomicLong failedVotes = new AtomicLong(0);
 
     final Callback callback = new Callback();
 
@@ -29,6 +41,8 @@ public class AnotherStreamClient extends BenchmarkComponent {
 
     public AnotherStreamClient(String args[]) {
         super(args);
+        int numContestants = VoterUtil.getScaledNumContestants(this.getScaleFactor());
+        this.switchboard = new PhoneCallGenerator(this.getClientId(), numContestants);
     }
 
     @Override
@@ -39,6 +53,7 @@ public class AnotherStreamClient extends BenchmarkComponent {
                 try {
                     runOnce();
                 } catch (Exception e) {
+                    failedVotes.incrementAndGet();
                 }
 
             } // WHILE
@@ -50,14 +65,23 @@ public class AnotherStreamClient extends BenchmarkComponent {
 
     @Override
     protected boolean runOnce() throws IOException {
-        return true;
+        // Get the next phone call
+        PhoneCallGenerator.PhoneCall call = switchboard.receive();
+
+        Client client = this.getClientHandle();
+        boolean response = client.callProcedure(callback,
+                                                "Vote",
+                                                call.voteId,
+                                                call.phoneNumber,
+                                                call.contestantNumber);
+        return response;
     }
 
     @Override
     public String[] getTransactionDisplayNames() {
         // Return an array of transaction names
         String procNames[] = new String[]{
-            //Vote.class.getSimpleName()
+             Vote.class.getSimpleName()
         };
         return (procNames);
     }
@@ -68,6 +92,31 @@ public class AnotherStreamClient extends BenchmarkComponent {
         public void clientCallback(ClientResponse clientResponse) {
             // Increment the BenchmarkComponent's internal counter on the
             // number of transactions that have been completed
+            incrementTransactionCounter(clientResponse, 0);
+            
+            // Keep track of state (optional)
+            if (clientResponse.getStatus() == Status.OK) {
+                VoltTable results[] = clientResponse.getResults();
+                assert(results.length == 1);
+                long status = results[0].asScalarLong();
+                if (status == VoterConstants.VOTE_SUCCESSFUL) {
+                    acceptedVotes.incrementAndGet();
+                }
+                else if (status == VoterConstants.ERR_INVALID_CONTESTANT) {
+                    badContestantVotes.incrementAndGet();
+                }
+                else if (status == VoterConstants.ERR_VOTER_OVER_VOTE_LIMIT) {
+                    badVoteCountVotes.incrementAndGet();
+                }
+            }
+            else if (clientResponse.getStatus() == Status.ABORT_UNEXPECTED) {
+                if (clientResponse.getException() != null) {
+                    clientResponse.getException().printStackTrace();
+                }
+                if (debug.val && clientResponse.getStatusString() != null) {
+                    LOG.warn(clientResponse.getStatusString());
+                }
+            }
         }
     } // END CLASS
 }
