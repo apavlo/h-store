9dfdc59537af0d4f6a2f359e0fd7ccf0230d0398
jmeehan16 <jmeehan16@gmail.com>
2014-02-23 16:51:42 -0500

    adding GetResults

---
+++ src/benchmarks/edu/brown/benchmark/wordcountsstore/procedures/GetResults.java
@@ -0,0 +1,33 @@
+package edu.brown.benchmark.wordcountsstore.procedures;
+
+import java.util.List;
+
+import org.voltdb.ProcInfo;
+import org.voltdb.SQLStmt;
+import org.voltdb.VoltProcedure;
+import org.voltdb.VoltTable;
+
+import edu.brown.hstore.conf.HStoreConf;
+import edu.brown.stream.Batch;
+import edu.brown.stream.Tuple;
+
+@ProcInfo (
+        singlePartition = true
+    )
+public class GetResults extends VoltProcedure {
+    
+    public final SQLStmt selectResultsStmt = new SQLStmt(
+            //"SELECT word, sum(num) FROM W_WORDS GROUP BY word ORDER BY word ASC;"
+    		"SELECT word, sum(num) FROM W_RESULTS GROUP BY word ORDER BY word ASC;"
+        );
+    
+    //public long run(String word, int time) 
+    public VoltTable[] run()
+    {
+       VoltTable[] results;
+       voltQueueSQL(selectResultsStmt);
+       results = voltExecuteSQL(true);
+
+       return results;
+    }
+}
