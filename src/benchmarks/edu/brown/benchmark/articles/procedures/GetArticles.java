713d5ea3cea601e38827b14235543892734574a4
Atreyee Maiti <atreyeemaiti@gmail.com>
2014-03-04 21:16:54 -0500

    procedure mapping

--- src/benchmarks/edu/brown/benchmark/articles/procedures/GetArticles.java
+++ src/benchmarks/edu/brown/benchmark/articles/procedures/GetArticles.java
@@ -5,10 +5,10 @@ import org.voltdb.VoltProcedure;
 import org.voltdb.VoltTable;
 
 public class GetArticles extends VoltProcedure{
-    public final SQLStmt GetArticles = new SQLStmt("SELECT * FROM ARTICLES");
+    public final SQLStmt GetArticles = new SQLStmt("SELECT * FROM ARTICLES where a_id > ?");
     
-    public VoltTable[] run() {
-        voltQueueSQL(GetArticles);
+    public VoltTable[] run(long a_id) {
+        voltQueueSQL(GetArticles, a_id);
         return (voltExecuteSQL());
     }   
 
