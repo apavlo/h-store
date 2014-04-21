56aa8b2a2a7c50f49821816be34c80a830d183ac
Atreyee Maiti <atreyeemaiti@gmail.com>
2014-04-21 14:18:44 -0400

    Reproducing the distributed anticache scenario

--- src/benchmarks/edu/brown/benchmark/users/procedures/GetUsers.java
+++ src/benchmarks/edu/brown/benchmark/users/procedures/GetUsers.java
@@ -15,16 +15,33 @@ public class GetUsers extends VoltProcedure{
 //	-- a_id            Article's ID
 //	-- u_id            User's ID
 //	-- c_text            Actual comment text
-	
-    
-    public final SQLStmt GetUsers = new SQLStmt("SELECT * FROM USERS");
-    
-    public VoltTable[] run() {
-    	System.out.println("Running procedure Get Users");
+    public final SQLStmt GetUser = new SQLStmt("SELECT u_attr01 FROM USERS WHERE U_ID=?");
+    public final SQLStmt UpdateUsers = new SQLStmt("UPDATE USERS SET u_attr01=? WHERE U_ID=?");
+
+    public VoltTable[] run(long u_id, long u_id2) {
+    	System.out.println("Running procedure Update "+u_id);
     	
-        voltQueueSQL(GetUsers);
-        VoltTable[] results = voltExecuteSQL(true);
+        voltQueueSQL(GetUser, u_id);
+        VoltTable[] results = voltExecuteSQL();
+        boolean adv = results[0].advanceRow();
+        //assert (adv);
+        String attr1 = results[0].getString(0);
+        voltQueueSQL(UpdateUsers, attr1, u_id2);
+        results = voltExecuteSQL(true); 
+        assert results.length == 1;
         return results;
     }   
+
+	
+    
+//    public final SQLStmt GetUsers = new SQLStmt("SELECT * FROM USERS");
+//    
+//    public VoltTable[] run() {
+//    	System.out.println("Running procedure Get Users");
+//    	
+//        voltQueueSQL(GetUsers);
+//        VoltTable[] results = voltExecuteSQL(true);
+//        return results;
+//    }   
 
 }
--- tests/frontend/org/voltdb/regressionsuites/TestUsersSuite.java
+++ tests/frontend/org/voltdb/regressionsuites/TestUsersSuite.java
@@ -1,18 +1,27 @@
 package org.voltdb.regressionsuites;
 
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.concurrent.TimeUnit;
+
 import junit.framework.Test;
 
 import org.voltdb.BackendTarget;
 import org.voltdb.CatalogContext;
+import org.voltdb.VoltSystemProcedure;
 import org.voltdb.VoltTable;
 import org.voltdb.client.Client;
 import org.voltdb.client.ClientResponse;
+import org.voltdb.sysprocs.EvictTuples;
+import org.voltdb.utils.VoltTableUtil;
 
+import edu.brown.HStoreSiteTestUtil.LatchableProcedureCallback;
 import edu.brown.benchmark.users.UsersConstants;
 import edu.brown.benchmark.users.UsersLoader;
 import edu.brown.benchmark.users.UsersProjectBuilder;
 import edu.brown.benchmark.users.procedures.GetUsers;
 import edu.brown.hstore.Hstoreservice.Status;
+import edu.brown.utils.StringUtil;
 
 /**
  * Simple test suite for the Users benchmark
@@ -21,6 +30,7 @@ public class TestUsersSuite extends RegressionSuite {
 
     private static final String PREFIX = "users";
     private static final int NUM_TUPLES = 10000;
+    private static final int NOTIFY_TIMEOUT = 2000; // ms
     
     /**
      * Constructor needed for JUnit. Should just pass on parameters to superclass.
@@ -73,29 +83,84 @@ public class TestUsersSuite extends RegressionSuite {
     public void testReadRecords() throws Exception {
         Client client = this.getClient();
         this.initializeDatabase(client, NUM_TUPLES);
-        
+        // Force an eviction
+        Map<Integer, VoltTable> evictResults = this.evictData(client);
+        for (int partition : evictResults.keySet()) {
+            System.err.println("Partition " + partition);
+            System.err.println(StringUtil.prefix("  ", VoltTableUtil.format(evictResults.get(partition))));
+        }
+        System.err.println("-------------------------------");
+
         String procName = GetUsers.class.getSimpleName();
-        ClientResponse cresponse = client.callProcedure(procName);
+        long user1 = 9999;
+        long user2 = 1;
+        Object params[] = { user1, user2  };
+        ClientResponse cresponse = client.callProcedure(procName, params);
+//        String query = "UPDATE USERS SET u_attr01= COUNT(*) FROM " + UsersConstants.TABLENAME_USERS;
+//        ClientResponse cresponse = client.callProcedure("@AdHoc", query);
+        
         assertNotNull(cresponse);
         assertEquals(Status.OK, cresponse.getStatus());
-        assertEquals(NUM_TUPLES, cresponse.getResults()[0].getRowCount());
+        assertEquals(1, cresponse.getResults()[0].getRowCount());
     }
-    
+
+    private Map<Integer, VoltTable> evictData(Client client) throws Exception {
+//      System.err.printf("Evicting data..."); 
+      String procName = VoltSystemProcedure.procCallName(EvictTuples.class);
+      CatalogContext catalogContext = this.getCatalogContext();
+      String tableNames[] = { UsersConstants.TABLENAME_USERS };
+      LatchableProcedureCallback callback = new LatchableProcedureCallback(1);
+//      long evictBytes[] = { Integer.MAX_VALUE };
+      long evictBytes[] = {1024 * 500};
+      int numBlocks[] = { 1 };
+      
+      int remote_partition = catalogContext.getAllPartitionIdArray()[1];
+      System.err.printf("Evicting data at partition %d...\n", remote_partition);
+      String children[] = null;
+          Object params[] = { remote_partition, tableNames, children, evictBytes, numBlocks };
+          boolean result = client.callProcedure(callback, procName, params);
+          assertTrue(result);
+      
+      
+      // Wait until they all finish
+      result = callback.latch.await(NOTIFY_TIMEOUT, TimeUnit.MILLISECONDS);
+      assertTrue(callback.toString(), result);
+      
+      // Construct a mapping BasePartition->VoltTable
+      Map<Integer, VoltTable> m = new TreeMap<Integer, VoltTable>();
+      for (ClientResponse cr : callback.responses) {
+          assertEquals(cr.toString(), Status.OK, cr.getStatus());
+          assertEquals(cr.toString(), 1, cr.getResults().length);
+          m.put(cr.getBasePartition(), cr.getResults()[0]);
+      } // FOR
+      assertEquals(1, m.size());
+//      System.err.printf("Finished evicting data.");
+      return (m);
+  }
+
 
     public static Test suite() {
         VoltServerConfig config = null;
         // the suite made here will all be using the tests from this class
         MultiConfigSuiteBuilder builder = new MultiConfigSuiteBuilder(TestUsersSuite.class);
-
+        builder.setGlobalConfParameter("site.exec_voltdb_procinfo", true);
+        builder.setGlobalConfParameter("site.anticache_enable", true);
+        builder.setGlobalConfParameter("site.anticache_profiling", true);
+        builder.setGlobalConfParameter("site.anticache_reset", true);
+        builder.setGlobalConfParameter("site.anticache_check_interval", Integer.MAX_VALUE);
+        builder.setGlobalConfParameter("site.anticache_batching", false);
+        builder.setGlobalConfParameter("site.anticache_threshold_mb", 128);
+        
         UsersProjectBuilder project = new UsersProjectBuilder();
         project.addAllDefaults();
+        project.markTableEvictable(UsersConstants.TABLENAME_USERS);
         
         boolean success;
         
         /////////////////////////////////////////////////////////////
         // CONFIG #1: 1 Local Site/Partition running on JNI backend
         /////////////////////////////////////////////////////////////
-        config = new LocalSingleProcessServer(PREFIX+"-1part.jar", 1, BackendTarget.NATIVE_EE_JNI);
+        config = new LocalSingleProcessServer(PREFIX+"-1part.jar", 2, BackendTarget.NATIVE_EE_JNI);
         success = config.compile(project);
         assert(success);
         builder.addServerConfig(config);
