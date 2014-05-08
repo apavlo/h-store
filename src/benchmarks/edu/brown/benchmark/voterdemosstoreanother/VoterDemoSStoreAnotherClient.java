3b1b104405c06c0040fac34780137975cb83ce3a
hawkwang <wanghao.buaa@gmail.com>
2014-05-08 15:02:46 -0400

    update to integrate VoteGenServer part

--- src/benchmarks/edu/brown/benchmark/voterdemosstoreanother/VoterDemoSStoreAnotherClient.java
+++ src/benchmarks/edu/brown/benchmark/voterdemosstoreanother/VoterDemoSStoreAnotherClient.java
@@ -30,18 +30,22 @@
 
 package edu.brown.benchmark.voterdemosstoreanother;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.UnknownHostException;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.apache.log4j.Logger;
 import org.voltdb.VoltTable;
+import org.voltdb.VoltTableRow;
 import org.voltdb.client.Client;
 import org.voltdb.client.ClientResponse;
 import org.voltdb.client.ProcedureCallback;
 
 import weka.classifiers.meta.Vote;
-
 import edu.brown.api.BenchmarkComponent;
 import edu.brown.benchmark.voterdemosstoreanother.procedures.GenerateLeaderboard;
 import edu.brown.hstore.Hstoreservice.Status;
@@ -54,7 +58,12 @@ public class VoterDemoSStoreAnotherClient extends BenchmarkComponent {
     private static int timestamp;
 
     // Phone number generator
-    PhoneCallGenerator switchboard;
+    //PhoneCallGenerator switchboard;
+    edu.brown.stream.VoteGenClient switchboard;
+
+    private String stat_filename;
+    public static AtomicLong count = new AtomicLong(0);
+    public static AtomicLong fixnum = new AtomicLong(10000l);
 
     // Flags to tell the worker threads to stop or go
     AtomicBoolean warmupComplete = new AtomicBoolean(false);
@@ -67,17 +76,22 @@ public class VoterDemoSStoreAnotherClient extends BenchmarkComponent {
     AtomicLong failedVotes = new AtomicLong(0);
 
     final Callback callback = new Callback();
-
+    final StatisticCallback stat_callback =  new StatisticCallback();
+    
     public static void main(String args[]) {
         BenchmarkComponent.main(VoterDemoSStoreAnotherClient.class, args, false);
     }
 
-    public VoterDemoSStoreAnotherClient(String args[]) {
+    public VoterDemoSStoreAnotherClient(String args[]) throws UnknownHostException, IOException {
         super(args);
         int numContestants = VoterDemoSStoreAnotherUtil.getScaledNumContestants(this.getScaleFactor());
-        this.switchboard = new PhoneCallGenerator(this.getClientId(), numContestants);
+        //this.switchboard = new PhoneCallGenerator(this.getClientId(), numContestants);
+        this.switchboard = new edu.brown.stream.VoteGenClient();
+        
         lastTime = System.nanoTime();
         timestamp = 0;
+        
+        stat_filename = "voterdemosstoreanother" + ".txt";
     }
 
     @Override
@@ -107,17 +121,55 @@ public class VoterDemoSStoreAnotherClient extends BenchmarkComponent {
         	timestamp++;
         }
     	
-        PhoneCallGenerator.PhoneCall call = switchboard.receive();
-
-        Client client = this.getClientHandle();
-        boolean response = client.callProcedure(callback,
-                                                "Vote",
-                                                call.voteId,
-                                                call.phoneNumber,
-                                                call.contestantNumber,
-                                                VoterDemoSStoreAnotherConstants.MAX_VOTES,
-                                                timestamp);
+    	boolean response = true;
+    	
+        synchronized (this) {  
+            //PhoneCallGenerator.PhoneCall call = switchboard.receive();
+            edu.brown.stream.VoteGenClient.CurrentCall call = switchboard.getNextCall();
+            if(call==null)
+                return true;
+            
+            System.out.println(" clientId: " + this.getClientId() + " - " + call.getString());
+            
+            Client client = this.getClientHandle();
+            response = client.callProcedure(callback,
+                                                    "Vote",
+                                                    call.voteId,
+                                                    call.phoneNumber,
+                                                    call.contestantNumber,
+                                                    VoterDemoSStoreAnotherConstants.MAX_VOTES,
+                                                    //call.timestamp);
+                                                    timestamp);
+            GetStatisticInfo();
+        }
         return response;
+    }
+    
+    private synchronized void GetStatisticInfo()
+    {
+        try
+        {
+            VoterDemoSStoreAnotherClient.count.incrementAndGet();
+            boolean beSame = (VoterDemoSStoreAnotherClient.count.get()==VoterDemoSStoreAnotherClient.fixnum.get());
+            //System.out.println(" clientId: " + this.getClientId() + " - fixnum: " + VoterDemoSStoreAnotherClient.fixnum.get() + "- count: " + VoterDemoSStoreAnotherClient.count.get() + " - " +  beSame);
+            
+            if( beSame==true )
+            {
+                //System.out.println("GetStatisticInfo() 1- " + String.valueOf(VoterDemoHStoreAnotherClient.fixnum));
+                //System.out.println("call GetStatisticInfo ...");
+                
+                Client client = this.getClientHandle();
+                client.callProcedure(stat_callback, "Results");
+
+                VoterDemoSStoreAnotherClient.fixnum.addAndGet(10000l);
+                //System.out.println("GetStatisticInfo() 2- " + String.valueOf(VoterDemoHStoreAnotherClient.fixnum));
+                
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
     }
 
     @Override
@@ -136,6 +188,8 @@ public class VoterDemoSStoreAnotherClient extends BenchmarkComponent {
         public void clientCallback(ClientResponse clientResponse) {
             // Increment the BenchmarkComponent's internal counter on the
             // number of transactions that have been completed
+            System.out.println(clientResponse.getStatus());
+            
             incrementTransactionCounter(clientResponse, 0);
             
             // Keep track of state (optional)
@@ -164,4 +218,47 @@ public class VoterDemoSStoreAnotherClient extends BenchmarkComponent {
             }
         }
     } // END CLASS
+    
+    private class StatisticCallback implements ProcedureCallback {
+        @Override
+        public void clientCallback(ClientResponse clientResponse)
+        {
+            if (clientResponse.getStatus() == Status.OK) {
+                VoltTable vt = clientResponse.getResults()[0];
+                
+                int row_len = vt.getRowCount();
+                
+                long fixnum = VoterDemoSStoreAnotherClient.fixnum.get();
+                String line =  String.valueOf(fixnum - 10000l) + ": ";
+                for(int i=0;i<row_len; i++)
+                {
+                    VoltTableRow row = vt.fetchRow(i);
+                    String contestant_name = row.getString(0);
+                    long total_votes = row.getLong(2);
+                    
+                    String content = contestant_name + "-" + String.valueOf(total_votes);
+                    
+                    line += content + " ";
+                }
+                
+                //System.out.println(line);
+                
+                try {
+                    WriteToFile(line);
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                
+            }            
+        }
+        
+        private synchronized void WriteToFile(String content) throws IOException
+        {
+            //System.out.println(stat_filename + " : " + content );
+            PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(stat_filename, true)));
+            out.println(content);
+            out.close();
+        }
+    }
 }
