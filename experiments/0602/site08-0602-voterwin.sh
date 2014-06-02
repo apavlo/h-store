97f3120fb3e7c69a206afcaeecb8ef255b625d7c
jmeehan16 <jmeehan16@gmail.com>
2014-06-02 19:17:07 -0400

    site08 experiments

---
+++ experiments/0602/site08-0602-voterwin.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+ant clean-all build-all
+ant hstore-prepare -Dproject=voterwintimehstore -Dhosts="localhost:0:0"
+ant hstore-prepare -Dproject=voterwintimesstore -Dhosts="localhost:0:0"
+ant hstore-prepare -Dproject=voterwinhstore -Dhosts="localhost:0:0"
+ant hstore-prepare -Dproject=voterwinsstore -Dhosts="localhost:0:0"
+python ./tools/autorunexp.py -p voterwinhstore -o "experiments/0602/voterwinhstore-1c-10000w1s-98-0602-site08.txt" --txnthreshold 0.98 -e "experiments/0602/site08-0602.txt" --winconfig "tuple 10000w1s (site08)" --threads 1 --rmin 1000 --rstep 1000 --finalrstep 100 --warmup 10000 --numruns 5 
+python ./tools/autorunexp.py -p voterwinsstore -o "experiments/0602/voterwinsstore-1c-10000w1s-98-0602-site08.txt" --txnthreshold 0.98 -e "experiments/0602/site08-0602.txt" --winconfig "tuple 10000w1s (site08)" --threads 1 --rmin 1000 --rstep 1000 --finalrstep 100 --warmup 10000 --numruns 5 
+python ./tools/autorunexp.py -p voterwintimehstore -o "experiments/0602/voterwintimehstore-1c-60w1s1000t-98-0602-site08.txt" --txnthreshold 0.98 -e "experiments/0602/site08-0602.txt" --winconfig "time 60w1s1000t (site08)" --threads 1 --rmin 1000 --rstep 1000 --finalrstep 100 --warmup 70000 --numruns 5 
+python ./tools/autorunexp.py -p voterwintimesstore -o "experiments/0602/voterwintimesstore-1c-60w1s1000t-98-0602-site08.txt" --txnthreshold 0.98 -e "experiments/0602/site08-0602.txt" --winconfig "time 60w1s1000t (site08)" --threads 1 --rmin 1000 --rstep 1000 --finalrstep 100 --warmup 70000 --numruns 5 
+
+python ./tools/autorunexp.py -p voterwinhstore -o "experiments/0602/voterwinhstore-1c-10000w1s-95-0602-site08.txt" --txnthreshold 0.95 -e "experiments/0602/site08-0602.txt" --winconfig "tuple 10000w1s (site08)" --threads 1 --rmin 1000 --rstep 1000 --finalrstep 100 --warmup 10000 --numruns 5 
+python ./tools/autorunexp.py -p voterwinsstore -o "experiments/0602/voterwinsstore-1c-10000w1s-95-0602-site08.txt" --txnthreshold 0.95 -e "experiments/0602/site08-0602.txt" --winconfig "tuple 10000w1s (site08)" --threads 1 --rmin 1000 --rstep 1000 --finalrstep 100 --warmup 10000 --numruns 5 
+python ./tools/autorunexp.py -p voterwintimehstore -o "experiments/0602/voterwintimehstore-1c-60w1s1000t-95-0602-site08.txt" --txnthreshold 0.95 -e "experiments/0602/site08-0602.txt" --winconfig "time 60w1s1000t (site08)" --threads 1 --rmin 1000 --rstep 1000 --finalrstep 100 --warmup 70000 --numruns 5 
+python ./tools/autorunexp.py -p voterwintimesstore -o "experiments/0602/voterwintimesstore-1c-60w1s1000t-95-0602-site08.txt" --txnthreshold 0.95 -e "experiments/0602/site08-0602.txt" --winconfig "time 60w1s1000t (site08)" --threads 1 --rmin 1000 --rstep 1000 --finalrstep 100 --warmup 70000 --numruns 5 
+
+python ./tools/autorunexp.py -p voterwinhstore -o "experiments/0602/voterwinhstore-1c-10000w1s-90-0602-site08.txt" --txnthreshold 0.90 -e "experiments/0602/site08-0602.txt" --winconfig "tuple 10000w1s (site08)" --threads 1 --rmin 1000 --rstep 1000 --finalrstep 100 --warmup 10000 --numruns 5 
+python ./tools/autorunexp.py -p voterwinsstore -o "experiments/0602/voterwinsstore-1c-10000w1s-90-0602-site08.txt" --txnthreshold 0.90 -e "experiments/0602/site08-0602.txt" --winconfig "tuple 10000w1s (site08)" --threads 1 --rmin 1000 --rstep 1000 --finalrstep 100 --warmup 10000 --numruns 5 
+python ./tools/autorunexp.py -p voterwintimehstore -o "experiments/0602/voterwintimehstore-1c-60w1s1000t-90-0602-site08.txt" --txnthreshold 0.90 -e "experiments/0602/site08-0602.txt" --winconfig "time 60w1s1000t (site08)" --threads 1 --rmin 1000 --rstep 1000 --finalrstep 100 --warmup 70000 --numruns 5 
+python ./tools/autorunexp.py -p voterwintimesstore -o "experiments/0602/voterwintimesstore-1c-60w1s1000t-90-0602-site08.txt" --txnthreshold 0.90 -e "experiments/0602/site08-0602.txt" --winconfig "time 60w1s1000t (site08)" --threads 1 --rmin 1000 --rstep 1000 --finalrstep 100 --warmup 70000 --numruns 5 
