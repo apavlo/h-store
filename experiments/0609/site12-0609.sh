2b0292c2b432e9778f1e434deac4819112ebd9f5
jmeehan16 <jmeehan16@gmail.com>
2014-06-09 15:03:53 -0400

    adding 0609 experiments

---
+++ experiments/0609/site12-0609.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+ant clean-all build-all
+#ant hstore-prepare -Dproject=voterwintimehstore -Dhosts="localhost:0:0"
+ant hstore-prepare -Dproject=voterwintimesstore -Dhosts="localhost:0:0"
+ant hstore-prepare -Dproject=voterwintimesstorewinonly -Dhosts="localhost:0:0"
+#ant hstore-prepare -Dproject=voterwinhstore -Dhosts="localhost:0:0"
+#ant hstore-prepare -Dproject=voterwinsstore -Dhosts="localhost:0:0"
+#python ./tools/autorunexp.py -p voterwinhstore -o "experiments/0609/voterwinhstore-1c-100w1s-98-0609-site12.txt" --txnthreshold 0.98 -e "experiments/0609/site12-0609.txt" --winconfig "tuple 100w1s (site12)" --threads 1 --rmin 1000 --rstep 1000 --finalrstep 100 --warmup 10000 --numruns 5 
+#python ./tools/autorunexp.py -p voterwinsstore -o "experiments/0609/voterwinsstore-1c-100w1s-98-0609-site12.txt" --txnthreshold 0.98 -e "experiments/0609/site12-0609.txt" --winconfig "tuple 100w1s (site12)" --threads 1 --rmin 1000 --rstep 1000 --finalrstep 100 --warmup 10000 --numruns 5 
+#python ./tools/autorunexp.py -p voterwintimehstore -o "experiments/0609/voterwintimehstore-1c-30w1s100t-98-0609-site12.txt" --txnthreshold 0.98 -e "experiments/0609/site12-0609.txt" --winconfig "time 30w1s100t (site12)" --threads 1 --rmin 1000 --rstep 1000 --finalrstep 100 --warmup 10000 --numruns 5 
+python ./tools/autorunexp.py -p voterwintimesstore -o "experiments/0609/voterwintimesstore-1c-30w1s100t-98-0609-site12.txt" --txnthreshold 0.98 -e "experiments/0609/site12-0609.txt" --winconfig "time 30w1s100t (site12)" --threads 1 --rmin 1000 --rstep 1000 --finalrstep 100 --warmup 10000 --numruns 5 
+python ./tools/autorunexp.py -p voterwintimesstorewinonly -o "experiments/0609/voterwintimesstorewinonly-1c-30w1s100t-98-0609-site12.txt" --txnthreshold 0.98 -e "experiments/0609/site12-0609.txt" --winconfig "time 30w1s100t (site12)" --threads 1 --rmin 1000 --rstep 1000 --finalrstep 100 --warmup 10000 --numruns 5 
+
+#python ./tools/autorunexp.py -p voterwinhstore -o "experiments/0609/voterwinhstore-1c-100w1s-95-0609-site12.txt" --txnthreshold 0.95 -e "experiments/0609/site12-0609.txt" --winconfig "tuple 100w1s (site12)" --threads 1 --rmin 1000 --rstep 1000 --finalrstep 100 --warmup 10000 --numruns 5 
+#python ./tools/autorunexp.py -p voterwinsstore -o "experiments/0609/voterwinsstore-1c-100w1s-95-0609-site12.txt" --txnthreshold 0.95 -e "experiments/0609/site12-0609.txt" --winconfig "tuple 100w1s (site12)" --threads 1 --rmin 1000 --rstep 1000 --finalrstep 100 --warmup 10000 --numruns 5 
+#python ./tools/autorunexp.py -p voterwintimehstore -o "experiments/0609/voterwintimehstore-1c-30w1s100t-95-0609-site12.txt" --txnthreshold 0.95 -e "experiments/0609/site12-0609.txt" --winconfig "time 30w1s100t (site12)" --threads 1 --rmin 1000 --rstep 1000 --finalrstep 100 --warmup 10000 --numruns 5 
+python ./tools/autorunexp.py -p voterwintimesstore -o "experiments/0609/voterwintimesstore-1c-30w1s100t-95-0609-site12.txt" --txnthreshold 0.95 -e "experiments/0609/site12-0609.txt" --winconfig "time 30w1s100t (site12)" --threads 1 --rmin 1000 --rstep 1000 --finalrstep 100 --warmup 10000 --numruns 5 
+python ./tools/autorunexp.py -p voterwintimesstorewinonly -o "experiments/0609/voterwintimesstorewinonly-1c-30w1s100t-95-0609-site12.txt" --txnthreshold 0.95 -e "experiments/0609/site12-0609.txt" --winconfig "time 30w1s100t (site12)" --threads 1 --rmin 1000 --rstep 1000 --finalrstep 100 --warmup 10000 --numruns 5 
+
+#python ./tools/autorunexp.py -p voterwinhstore -o "experiments/0609/voterwinhstore-1c-100w1s-90-0609-site12.txt" --txnthreshold 0.90 -e "experiments/0609/site12-0609.txt" --winconfig "tuple 100w1s (site12)" --threads 1 --rmin 1000 --rstep 1000 --finalrstep 100 --warmup 10000 --numruns 5 
+#python ./tools/autorunexp.py -p voterwinsstore -o "experiments/0609/voterwinsstore-1c-100w1s-90-0609-site12.txt" --txnthreshold 0.90 -e "experiments/0609/site12-0609.txt" --winconfig "tuple 100w1s (site12)" --threads 1 --rmin 1000 --rstep 1000 --finalrstep 100 --warmup 10000 --numruns 5 
+#python ./tools/autorunexp.py -p voterwintimehstore -o "experiments/0609/voterwintimehstore-1c-30w1s100t-90-0609-site12.txt" --txnthreshold 0.90 -e "experiments/0609/site12-0609.txt" --winconfig "time 30w1s100t (site12)" --threads 1 --rmin 1000 --rstep 1000 --finalrstep 100 --warmup 10000 --numruns 5 
+python ./tools/autorunexp.py -p voterwintimesstore -o "experiments/0609/voterwintimesstore-1c-30w1s100t-90-0609-site12.txt" --txnthreshold 0.90 -e "experiments/0609/site12-0609.txt" --winconfig "time 30w1s100t (site12)" --threads 1 --rmin 1000 --rstep 1000 --finalrstep 100 --warmup 10000 --numruns 5 
+python ./tools/autorunexp.py -p voterwintimesstorewinonly -o "experiments/0609/voterwintimesstorewinonly-1c-30w1s100t-90-0609-site12.txt" --txnthreshold 0.90 -e "experiments/0609/site12-0609.txt" --winconfig "time 30w1s100t (site12)" --threads 1 --rmin 1000 --rstep 1000 --finalrstep 100 --warmup 10000 --numruns 5 
