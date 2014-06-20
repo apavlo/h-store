d82003fe964945aaab0df0a037087e08f27fc03c
Erik Sutherland <esuther@pdx.com>
2014-06-19 18:55:42 -0700

    Quelled the complexity in bikerstream

--- src/benchmarks/edu/brown/benchmark/bikerstream/procedures/CheckoutBike.java
+++ src/benchmarks/edu/brown/benchmark/bikerstream/procedures/CheckoutBike.java
@@ -41,115 +41,50 @@ import edu.brown.benchmark.bikerstream.BikerStreamConstants;
 @ProcInfo (
     singlePartition = true
 )
-
 public class CheckoutBike extends VoltProcedure {
 
     // Logging Information
-    private static final Logger Log = Logger.getLogger(ReserveBike.class);
+    private static final Logger Log = Logger.getLogger(CheckoutBike.class);
     // Is debugging on or not?
     final boolean debug = Log.isDebugEnabled();
 
-    // Get the reservation information from given a rider_id
-    public final SQLStmt checkReservation = new SQLStmt(
-                "SELECT * FROM bikes WHERE last_rider = ? AND state = 'RESERVED';"
+    public final SQLStmt getStation = new SQLStmt(
+                "SELECT * FROM stations where station_id = ?"
             );
 
-    // Invalidate a previous reservation in the reservation table
-    public final SQLStmt invalidateReservation = new SQLStmt(
-                "UPDATE bikeres SET valid=0 WHERE rider_id = ? AND bike_id = ?;"
+    public final SQLStmt updateStation = new SQLStmt(
+                "UPDATE stations SET num_bikes = ?, num_docks = ? where station_id = ?"
             );
 
-    // Modify the state of the bike to reflect that it has been checked out
-    public final SQLStmt changeBikeState = new SQLStmt(
-                "UPDATE bikes " +
-                "SET state = 'OUT', last_rider = ? " +
-                "WHERE bike_id = ?;"
+    public final SQLStmt logSuccess = new SQLStmt(
+                "INSERT INTO logs (rider_id, time, success, action) VALUES (?,?,1,?)"
             );
 
-    // Get all availible bikes for checkout without a reservation
-    public final SQLStmt getAllAvailibleBikes = new SQLStmt(
-                "SELECT * FROM bikes WHERE state = 'DOCKED';"
-            );
-
-    // Flip the occupied bit in the docks table
-    public final SQLStmt removeFromDock = new SQLStmt(
-                "UPDATE docks SET occupied = 0 WHERE dock_id = ?;"
+    public final SQLStmt logFailure = new SQLStmt(
+                "INSERT INTO logs (rider_id, time, success, action) VALUES (?,?,0,?)"
             );
 
 
-    public long run(long rider_id) {
+    public long run(long rider_id, long station_id) throws Exception {
 
-        // Lets pull up our reservation
-        voltQueueSQL(checkReservation, rider_id);
-        VoltTable[] results = voltExecuteSQL();
+        voltQueueSQL(getStation, station_id);
+        VoltTable results[] = voltExecuteSQL();
 
-        if (results[0].getRowCount() == 1) { // Found a Reservation
-            // We have found our reservation, from here it should be easy to find and
-            // grab our bike, then set everything back so that others may use our dock
-            // to dock a bike.
+        assert(results[0].getRowCount() == 1);
 
-            long bike_id = results[0].fetchRow(0).getLong("bike_id");
-            long dock_id = results[0].fetchRow(0).getLong("last_dock");
+        long numBikes = results[0].fetchRow(0).getLong("num_bikes");
+        long numDocks = results[0].fetchRow(0).getLong("num_docks");
 
-            // Change the reservation table and invalidate the reservation
-            voltQueueSQL(invalidateReservation, rider_id, bike_id);
-
-            // Change the status of the bike to riding
-            voltQueueSQL(changeBikeState, rider_id, bike_id);
-
-            // Change the dock occupied status
-            voltQueueSQL(removeFromDock, dock_id);
-
-            voltExecuteSQL(true);
-            return BikerStreamConstants.BIKE_CHECKEDOUT;
-
-
-        } else if (results[0].getRowCount() == 0) { // No reservation on file (anymore?)
-            // Here we have tp checkout a bike without having a reservation first
-            // This is going to be tricky cause who knows if someone else will take
-            // a bike out from under us in the middle of the process
-
-            int attempts = 0;
-
-            do { // Try to checkout a bike a few times
-
-                voltQueueSQL(getAllAvailibleBikes);
-                results = voltExecuteSQL();
-
-                // Make sure there are bikes available
-                int numOfBikes = results[0].getRowCount();
-
-                if (numOfBikes > 0 ){
-                    // Generate an index for one of the bikes in the returned
-                    // table.
-                    int  idx     = (int) (Math.random() * (float) numOfBikes);
-
-                    // Return the id of an availible bike
-                    long bike_id = results[0].fetchRow(idx).getLong("bike_id");
-                    long dock_id = results[0].fetchRow(idx).getLong("last_dock");
-
-                    // Update the state of the bike reflecting the 'checkout'
-                    voltQueueSQL(changeBikeState, rider_id, bike_id);
-
-                    // Change the dock occupied status
-                    voltQueueSQL(removeFromDock, dock_id);
-
-                    results = voltExecuteSQL(true);
-
-                    return BikerStreamConstants.BIKE_CHECKEDOUT;
-                }
-
-            } while (attempts++ < BikerStreamConstants.MAX_CHECKOUT_ATTEMPTS);
-
-
-        } else { // FATAL WEIRDNESS (must have multiple bikes reserved)
-            throw new RuntimeException(
-                    "[Checkout] Rider: " + rider_id +
-                    " had a reservation Error (multiple reservations?)"
-                    );
+        if (numBikes > 0){
+            voltQueueSQL(updateStation, --numBikes, ++numDocks, station_id);
+            voltQueueSQL(logSuccess, rider_id, new TimestampType(), "successfully got bike from station: " + station_id);
+            voltExecuteSQL();
+            return 1;
+        } else {
+            voltQueueSQL(logFailure, rider_id, new TimestampType(), "could not get bike from station: " + station_id);
+            throw new RuntimeException("There are no bikes availible at station: " + station_id);
         }
 
-        return BikerStreamConstants.BIKE_NOT_CHECKEDOUT;
     }
 
 } // End Class
