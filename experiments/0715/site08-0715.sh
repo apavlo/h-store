f5b0f784a29bbd5f8634e37897d3cbe697454a42
jmeehan16 <jmeehan16@gmail.com>
2014-07-15 10:27:02 -0400

    site08 experiments

--- experiments/0715/site08-0715.sh
+++ experiments/0715/site08-0715.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#ant clean-all build-all
+ant clean-all build-all
 BENCH=("winhstore" "winhstorenocleanup" "winhstorenostate" "winsstore")
 NEWW=("100" "1000" "10000")
 NEWS=("1" "5" "10" "100")
@@ -12,7 +12,7 @@ for d in "${BENCH[@]}"
 do
 REP="w${w}s${s}"
 ant hstore-prepare -Dproject="voter${d}${REP}" -Dhosts="localhost:0:0"
-python ./tools/autorunexp.py -p "voter${d}${REP}" -o "experiments/0715/voter${d}${REP}-1c-95-0915-site09.txt" --txnthreshold 0.95 -e "experiments/0715/site09-0715.txt" --winconfig "tuple ${REP} (site09)" --threads 1 --rmin 1000 --rstep 1000 --finalrstep 100 --warmup 10000 --numruns 1 
+python ./tools/autorunexp.py -p "voter${d}${REP}" -o "experiments/0715/voter${d}${REP}-1c-95-0915-site08.txt" --txnthreshold 0.95 -e "experiments/0715/site08-0715.txt" --winconfig "tuple ${REP} (site08)" --threads 1 --rmin 1000 --rstep 1000 --finalrstep 100 --warmup 10000 --numruns 1 
 done
 done
 done
