fa066291ab1e55d2bd402a2f92bef9c2e14c4fc8
jmeehan16 <jmeehan16@gmail.com>
2014-07-20 17:45:42 -0400

    added experiment

--- experiments/0720/site08-0720-log.sh
+++ experiments/0720/site08-0720-log.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 ant clean-all build-all
+ant hstore-prepare -Dproject="voterdemohstorewXsYY" -Dhosts="localhost:0:0"
+ant hstore-prepare -Dproject="voterdemosstorewXsYY" -Dhosts="localhost:0:0"
+python ./tools/autorunexp.py -p "voterdemohstorewXsYY" -o "experiments/0720/voterdemohstorewXsYY-1c-90-0720-site08-log.txt" --txnthreshold 0.90 -e "experiments/0720/site08-0720-log.txt" --winconfig "tuple w100s10 (site08)" --threads 1 --rmin 1000 --rstep 1000 --finalrstep 100 --warmup 10000 --numruns 1 --log
+python ./tools/autorunexp.py -p "voterdemosstorewXsYY" -o "experiments/0720/voterdemosstorewXsYY-1c-90-0720-site08-log.txt" --txnthreshold 0.90 -e "experiments/0720/site08-0720-log.txt" --winconfig "tuple w100s10 (site08)" --threads 1 --rmin 1000 --rstep 1000 --finalrstep 100 --warmup 10000 --numruns 1 --log
+
 BENCH=("winhstore" "winhstorenocleanup" "winhstorenostate" "winsstore")
 NEWW=("100" "1000" "10000")
 NEWS=("1" "5" "10" "100")
