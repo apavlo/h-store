73fda4fda955daad0577450140405d7b26b5bde9
jmeehan16 <jmeehan16@gmail.com>
2014-07-20 17:13:20 -0400

    added experiment

---
+++ experiments/0720/site10-0720.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ant clean-all build-all
+ant hstore-prepare -Dproject="voterdemohstorewXsYY" -Dhosts="localhost:0:0"
+ant hstore-prepare -Dproject="voterdemosstorewXsYY" -Dhosts="localhost:0:0"
+python ./tools/autorunexp.py -p "voterdemohstorewXsYY" -o "experiments/0720/voterdemohstorewXsYY-1c-90-0720-site10.txt" --txnthreshold 0.90 -e "experiments/0720/site10-0720.txt" --winconfig "tuple w100s10 (site10)" --threads 1 --rmin 1000 --rstep 1000 --finalrstep 100 --warmup 10000 --numruns 1
+python ./tools/autorunexp.py -p "voterdemosstorewXsYY" -o "experiments/0720/voterdemosstorewXsYY-1c-90-0720-site10.txt" --txnthreshold 0.90 -e "experiments/0720/site10-0720.txt" --winconfig "tuple w100s10 (site10)" --threads 1 --rmin 1000 --rstep 1000 --finalrstep 100 --warmup 10000 --numruns 1
