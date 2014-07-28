2f3f2cba33c2ac82ecb7b176056aec2d5af3f361
jmeehan16 <jmeehan16@gmail.com>
2014-07-28 15:50:23 -0400

    committing experiments

--- experiments/0727/site08-0727-groupcommit.sh
+++ experiments/0727/site08-0727-groupcommit.sh
@@ -3,10 +3,10 @@ ant hstore-prepare -Dproject="voterdemohstorewXsYY" -Dhosts="localhost:0:0"
 ant hstore-prepare -Dproject="voterdemosstorewXsYY" -Dhosts="localhost:0:0"
 
 #h-store
-python ./tools/autorunexp.py -p "voterdemohstorewXsYY" -o "experiments/0727/voterdemohstorewXsYY-1c-90-0727-site08.txt" --txnthreshold 0.90 -e "experiments/0727/site08-0727-groupcommit.txt" \
---winconfig "tuple w100s10 (site08)" --threads 1 --rmin 100 --rstep 100 --finalrstep 10 --warmup 10000 --log --hstore --hscheduler --numruns 1 
-python ./tools/autorunexp.py -p "voterdemohstorewXsYY" -o "experiments/0727/voterdemohstorewXsYY-1c-90-0727-site08.txt" --txnthreshold 0.90 -e "experiments/0727/site08-0727-groupcommit.txt" \
---winconfig "tuple w100s10 (site08)" --threads 1 --rmin 100 --rstep 100 --finalrstep 10 --warmup 10000 --log --groupcommit 0 --hstore --hscheduler --numruns 1 
+#python ./tools/autorunexp.py -p "voterdemohstorewXsYY" -o "experiments/0727/voterdemohstorewXsYY-1c-90-0727-site08.txt" --txnthreshold 0.90 -e "experiments/0727/site08-0727-groupcommit.txt" \
+#--winconfig "tuple w100s10 (site08)" --threads 1 --rmin 100 --rstep 100 --finalrstep 10 --warmup 10000 --log --hstore --hscheduler --numruns 1 
+#python ./tools/autorunexp.py -p "voterdemohstorewXsYY" -o "experiments/0727/voterdemohstorewXsYY-1c-90-0727-site08.txt" --txnthreshold 0.90 -e "experiments/0727/site08-0727-groupcommit.txt" \
+#--winconfig "tuple w100s10 (site08)" --threads 1 --rmin 100 --rstep 100 --finalrstep 10 --warmup 10000 --log --groupcommit 0 --hstore --hscheduler --numruns 1 
 python ./tools/autorunexp.py -p "voterdemohstorewXsYY" -o "experiments/0727/voterdemohstorewXsYY-1c-90-0727-site08.txt" --txnthreshold 0.90 -e "experiments/0727/site08-0727-groupcommit.txt" \
 --winconfig "tuple w100s10 (site08)" --threads 1 --rmin 100 --rstep 100 --finalrstep 10 --warmup 10000 --log --groupcommit 10 --hstore --hscheduler --numruns 1
 python ./tools/autorunexp.py -p "voterdemohstorewXsYY" -o "experiments/0727/voterdemohstorewXsYY-1c-90-0727-site08.txt" --txnthreshold 0.90 -e "experiments/0727/site08-0727-groupcommit.txt" \
@@ -19,8 +19,8 @@ python ./tools/autorunexp.py -p "voterdemohstorewXsYY" -o "experiments/0727/vote
 #s-store weak recovery
 python ./tools/autorunexp.py -p "voterdemosstorewXsYY" -o "experiments/0727/voterdemosstorewXsYY-1c-90-0727-site08.txt" --txnthreshold 0.90 -e "experiments/0727/site08-0727-groupcommit.txt" \
 --winconfig "tuple w100s10 (site08)" --threads 1 --rmin 100 --rstep 100 --finalrstep 10 --warmup 10000 --log --hscheduler --numruns 1 
-python ./tools/autorunexp.py -p "voterdemosstorewXsYY" -o "experiments/0727/voterdemosstorewXsYY-1c-90-0727-site08.txt" --txnthreshold 0.90 -e "experiments/0727/site08-0727-groupcommit.txt" \
---winconfig "tuple w100s10 (site08)" --threads 1 --rmin 100 --rstep 100 --finalrstep 10 --warmup 10000 --log --groupcommit 0 --hscheduler --numruns 1 
+#python ./tools/autorunexp.py -p "voterdemosstorewXsYY" -o "experiments/0727/voterdemosstorewXsYY-1c-90-0727-site08.txt" --txnthreshold 0.90 -e "experiments/0727/site08-0727-groupcommit.txt" \
+#--winconfig "tuple w100s10 (site08)" --threads 1 --rmin 100 --rstep 100 --finalrstep 10 --warmup 10000 --log --groupcommit 0 --hscheduler --numruns 1 
 python ./tools/autorunexp.py -p "voterdemosstorewXsYY" -o "experiments/0727/voterdemosstorewXsYY-1c-90-0727-site08.txt" --txnthreshold 0.90 -e "experiments/0727/site08-0727-groupcommit.txt" \
 --winconfig "tuple w100s10 (site08)" --threads 1 --rmin 100 --rstep 100 --finalrstep 10 --warmup 10000 --log --groupcommit 10 --hscheduler --numruns 1 
 python ./tools/autorunexp.py -p "voterdemosstorewXsYY" -o "experiments/0727/voterdemosstorewXsYY-1c-90-0727-site08.txt" --txnthreshold 0.90 -e "experiments/0727/site08-0727-groupcommit.txt" \
@@ -33,8 +33,8 @@ python ./tools/autorunexp.py -p "voterdemosstorewXsYY" -o "experiments/0727/vote
 #s-store strong recovery
 python ./tools/autorunexp.py -p "voterdemosstorewXsYY" -o "experiments/0727/voterdemosstorewXsYY-1c-90-0727-site08.txt" --txnthreshold 0.90 -e "experiments/0727/site08-0727-groupcommit.txt" \
 --winconfig "tuple w100s10 (site08)" --threads 1 --rmin 100 --rstep 100 --finalrstep 10 --warmup 10000 --log --hscheduler --weakrecovery_off --numruns 1 
-python ./tools/autorunexp.py -p "voterdemosstorewXsYY" -o "experiments/0727/voterdemosstorewXsYY-1c-90-0727-site08.txt" --txnthreshold 0.90 -e "experiments/0727/site08-0727-groupcommit.txt" \
---winconfig "tuple w100s10 (site08)" --threads 1 --rmin 100 --rstep 100 --finalrstep 10 --warmup 10000 --log --groupcommit 0 --hscheduler --weakrecovery_off --numruns 1 
+#python ./tools/autorunexp.py -p "voterdemosstorewXsYY" -o "experiments/0727/voterdemosstorewXsYY-1c-90-0727-site08.txt" --txnthreshold 0.90 -e "experiments/0727/site08-0727-groupcommit.txt" \
+#--winconfig "tuple w100s10 (site08)" --threads 1 --rmin 100 --rstep 100 --finalrstep 10 --warmup 10000 --log --groupcommit 0 --hscheduler --weakrecovery_off --numruns 1 
 python ./tools/autorunexp.py -p "voterdemosstorewXsYY" -o "experiments/0727/voterdemosstorewXsYY-1c-90-0727-site08.txt" --txnthreshold 0.90 -e "experiments/0727/site08-0727-groupcommit.txt" \
 --winconfig "tuple w100s10 (site08)" --threads 1 --rmin 100 --rstep 100 --finalrstep 10 --warmup 10000 --log --groupcommit 10 --hscheduler --weakrecovery_off --numruns 1 
 python ./tools/autorunexp.py -p "voterdemosstorewXsYY" -o "experiments/0727/voterdemosstorewXsYY-1c-90-0727-site08.txt" --txnthreshold 0.90 -e "experiments/0727/site08-0727-groupcommit.txt" \
