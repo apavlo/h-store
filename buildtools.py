8efc6938986a343eb407aa58946d1996f50c2205
Lin Ma <malin1993ml@gmail.com>
2014-08-17 16:23:15 -0400

    All right. I forgot to turn on the anticache_build.

--- buildtools.py
+++ buildtools.py
@@ -41,7 +41,7 @@ class BuildContext:
         self.LOG_LEVEL = "DEBUG"
         self.VOLT_LOG_LEVEL = None
         self.STORAGE_MMAP = False
-        self.ANTICACHE_BUILD = False
+        self.ANTICACHE_BUILD = True
         self.ANTICACHE_REVERSIBLE_LRU = True 
         self.ANTICACHE_NVM = False
         self.ANTICACHE_DRAM = False
