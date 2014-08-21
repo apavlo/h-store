604efddf180dbb6479f63a43e091bc9b624561d1
jmeehan16 <jmeehan16@gmail.com>
2014-08-21 17:57:05 -0400

    callProcedure experiments

--- experiments/0820/vm-proctests.sh
+++ experiments/0820/vm-proctests.sh
@@ -10,26 +10,26 @@ ant hstore-prepare -Dproject="microexpnoftriggerstrig4" -Dhosts="localhost:0:0"
 
 python ./tools/autorunexp.py -p "microexpftriggerstrig1" -o "experiments/0820/microexpftriggerstrig1-1c-95-0820-site07.txt" \
 --txnthreshold 0.95 -e "experiments/0820/site07-0820-proctests.txt" --winconfig "callproc params" \
---threads 1 --rmin 10000 --rstep 10000 --finalrstep 100 --warmup 10000 --hscheduler --numruns 1 --perc_compare
+--threads 1 --rmin 10000 --rstep 10000 --finalrstep 100 --warmup 10000 --hscheduler --numruns 3 --perc_compare
 python ./tools/autorunexp.py -p "microexpftriggerstrig2" -o "experiments/0820/microexpftriggerstrig2-1c-95-0820-site07.txt" \
 --txnthreshold 0.95 -e "experiments/0820/site07-0820-proctests.txt" --winconfig "callproc no params" \
---threads 1 --rmin 10000 --rstep 10000 --finalrstep 100 --warmup 10000 --hscheduler --numruns 1 --perc_compare
+--threads 1 --rmin 10000 --rstep 10000 --finalrstep 100 --warmup 10000 --hscheduler --numruns 3 --perc_compare
 python ./tools/autorunexp.py -p "microexpftriggerstrig3" -o "experiments/0820/microexpftriggerstrig3-1c-95-0820-site07.txt" \
 --txnthreshold 0.95 -e "experiments/0820/site07-0820-proctests.txt" --winconfig "callstreamproc params" \
---threads 1 --rmin 10000 --rstep 10000 --finalrstep 100 --warmup 10000 --hscheduler --numruns 1 --perc_compare
+--threads 1 --rmin 10000 --rstep 10000 --finalrstep 100 --warmup 10000 --hscheduler --numruns 3 --perc_compare
 python ./tools/autorunexp.py -p "microexpftriggerstrig4" -o "experiments/0820/microexpftriggerstrig4-1c-95-0820-site07.txt" \
 --txnthreshold 0.95 -e "experiments/0820/site07-0820-proctests.txt" --winconfig "callstreamproc no params" \
---threads 1 --rmin 10000 --rstep 10000 --finalrstep 100 --warmup 10000 --hscheduler --numruns 1 --perc_compare
+--threads 1 --rmin 10000 --rstep 10000 --finalrstep 100 --warmup 10000 --hscheduler --numruns 3 --perc_compare
 
 python ./tools/autorunexp.py -p "microexpnoftriggerstrig1" -o "experiments/0820/microexpnoftriggerstrig1-1c-95-0820-site07.txt" \
 --txnthreshold 0.95 -e "experiments/0820/site07-0820-proctests.txt" --winconfig "callproc params" \
---threads 1 --rmin 10000 --rstep 10000 --finalrstep 100 --warmup 10000 --hscheduler --numruns 1 --perc_compare
+--threads 1 --rmin 10000 --rstep 10000 --finalrstep 100 --warmup 10000 --hscheduler --numruns 3 --perc_compare
 python ./tools/autorunexp.py -p "microexpnoftriggerstrig2" -o "experiments/0820/microexpnoftriggerstrig2-1c-95-0820-site07.txt" \
 --txnthreshold 0.95 -e "experiments/0820/site07-0820-proctests.txt" --winconfig "callproc no params" \
---threads 1 --rmin 10000 --rstep 10000 --finalrstep 100 --warmup 10000 --hscheduler --numruns 1 --perc_compare
+--threads 1 --rmin 10000 --rstep 10000 --finalrstep 100 --warmup 10000 --hscheduler --numruns 3 --perc_compare
 python ./tools/autorunexp.py -p "microexpnoftriggerstrig3" -o "experiments/0820/microexpnoftriggerstrig3-1c-95-0820-site07.txt" \
 --txnthreshold 0.95 -e "experiments/0820/site07-0820-proctests.txt" --winconfig "callstreamproc params" \
---threads 1 --rmin 10000 --rstep 10000 --finalrstep 100 --warmup 10000 --hscheduler --numruns 1 --perc_compare
+--threads 1 --rmin 10000 --rstep 10000 --finalrstep 100 --warmup 10000 --hscheduler --numruns 3 --perc_compare
 python ./tools/autorunexp.py -p "microexpnoftriggerstrig4" -o "experiments/0820/microexpnoftriggerstrig4-1c-95-0820-site07.txt" \
 --txnthreshold 0.95 -e "experiments/0820/site07-0820-proctests.txt" --winconfig "callstreamproc no params" \
---threads 1 --rmin 10000 --rstep 10000 --finalrstep 100 --warmup 10000 --hscheduler --numruns 1 --perc_compare
+--threads 1 --rmin 10000 --rstep 10000 --finalrstep 100 --warmup 10000 --hscheduler --numruns 3 --perc_compare
