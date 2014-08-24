14e65169ebde40942ed7087578251bb7593f1236
john <john@cs.brown.edu>
2014-08-24 18:34:25 -0400

    added to compile script

--- demo/compiledemo.sh
+++ demo/compiledemo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+git pull
 ant clean-java build-java
 ant hstore-prepare -Dproject=voterdemosstorecorrect -Dhosts="localhost:0:0"
 ant hstore-prepare -Dproject=voterdemohstorecorrect -Dhosts="localhost:0:0"
