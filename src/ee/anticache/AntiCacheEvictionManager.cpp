0936df6b5fda56378fc1fca7f2f26c83ca47ad1d
Lin Ma <malin1993ml@gmail.com>
2015-11-29 09:42:38 -0800

    Revert MAX_EVICTED_TUPLE_SIZE.

--- src/ee/anticache/AntiCacheEvictionManager.cpp
+++ src/ee/anticache/AntiCacheEvictionManager.cpp
@@ -47,8 +47,8 @@
 #include <time.h>
 #include <stdlib.h>
 // FIXME: This is relatively small. 2500 might be a better guess
-#define MAX_EVICTED_TUPLE_SIZE 1060
-//#define MAX_EVICTED_TUPLE_SIZE 2500
+//#define MAX_EVICTED_TUPLE_SIZE 1060
+#define MAX_EVICTED_TUPLE_SIZE 2500
 
 namespace voltdb
 {
